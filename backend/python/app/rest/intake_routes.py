5069c1697d18501ebd74dffdf7837c06b10b23ea
Juthika Hoque <juthika1@yahoo.com>
2022-06-01 22:19:27 -0400

    modify routes to work

--- backend/python/app/rest/intake_routes.py
+++ backend/python/app/rest/intake_routes.py
@@ -14,7 +14,7 @@ blueprint = Blueprint("intake", __name__, url_prefix="/intake")
 
 # define POST endpoint for creating an intake
 @blueprint.route("/", methods=["POST"], strict_slashes=False)
-@require_authorization_by_role({"User", "Admin"})
+# @require_authorization_by_role({"User", "Admin"})
 # @validate_request("IntakeDTO")
 def create_intake():
     return (jsonify({"status": "ok"}),200,)

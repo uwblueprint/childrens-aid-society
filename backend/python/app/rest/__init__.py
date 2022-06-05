f686abe4223b247bfda5771d3ec323fb1b1ee43d
juthikahoque <77185514+juthikahoque@users.noreply.github.com>
2022-06-05 11:46:02 -0400

    Added dummy intake route (Julianne + Juthika) (#11)
    * add files for creating dummy intake routes
    
    * test
    
    * add intake dummy route
    
    * modify routes to work
    
    * lint
    
    * merge main
    
    * modify routes to work
    
    * lint
    
    * add files for creating dummy intake routes
    
    * add intake dummy route
    
    * modify routes to work
    
    * make modifications left in review
    
    * make modifications left in review
    
    * Modified intake routes/service files after review
    
    * added linting to changes
    
    Co-authored-by: Julianne Jorda <julianne.jorda@yahoo.com>
    Co-authored-by: Julianne Jorda <58669961+juliannejorda@users.noreply.github.com>

--- backend/python/app/rest/__init__.py
+++ backend/python/app/rest/__init__.py
@@ -3,11 +3,13 @@ def init_app(app):
         auth_routes,
         documentation_routes,
         entity_routes,
+        intake_routes,
         user_routes,
         visit_routes,
     )
 
     app.register_blueprint(user_routes.blueprint)
+    app.register_blueprint(intake_routes.blueprint)
     app.register_blueprint(auth_routes.blueprint)
     app.register_blueprint(entity_routes.blueprint)
     app.register_blueprint(documentation_routes.blueprint)

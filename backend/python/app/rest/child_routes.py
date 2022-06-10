f7aa745d4612e49c354a195e489fd62851755788
Atharva Gangal <70596721+thrvg@users.noreply.github.com>
2022-06-10 19:35:03 -0400

    Update child_routes.py

--- backend/python/app/rest/child_routes.py
+++ backend/python/app/rest/child_routes.py
@@ -9,9 +9,7 @@ from ..services.implementations.child_service import ChildService
 
 child_service = ChildService(current_app.logger)
 
-
 blueprint = Blueprint("child", __name__, url_prefix="/children")
-
 
 @blueprint.route("/", methods=["POST"], strict_slashes=False)
 @require_authorization_by_role({"Admin"})

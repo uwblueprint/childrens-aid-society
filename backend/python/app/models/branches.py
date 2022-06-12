55512aaf99119d6df5dc3ef0c4f49d6c0cd9b9a3
Juthika Hoque <juthika1@yahoo.com>
2022-06-12 11:34:28 -0400

    making changes based on reviews + fix upgrade/downgrade

--- backend/python/app/models/branches.py
+++ backend/python/app/models/branches.py
@@ -11,7 +11,6 @@ class Branches(db.Model):
     branch = db.Column(db.String, nullable=False)
 
     def to_dict(self, include_relationships=False):
-        # define the entities table
         cls = type(self)
 
         mapper = inspect(cls)
@@ -19,13 +18,8 @@ class Branches(db.Model):
         for column in mapper.attrs:
             field = column.key
             attr = getattr(self, field)
-            # if it's a regular column, extract the value
             if isinstance(column, ColumnProperty):
                 formatted[field] = attr
-            # otherwise, it's a relationship field
-            # (currently not applicable, but may be useful for entity groups)
             elif include_relationships:
-                # recursively format the relationship
-                # don't format the relationship's relationships
                 formatted[field] = [obj.to_dict() for obj in attr]
         return formatted

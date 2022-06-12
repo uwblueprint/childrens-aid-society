51cbd41c2b38d99bf2a29902d1233b2ddfcab025
Jennifer Chen <jenn.chenn93@gmail.com>
2022-06-12 15:39:49 -0400

    Change add new transport method implementation

--- backend/python/app/resources/transportation_method_dto.py
+++ backend/python/app/resources/transportation_method_dto.py
@@ -1,17 +1,18 @@
 class TransportationMethodDTO:
     def __init__(self, id, transportation_method):
         self.id = id
-        self.transportation_method = transportation_method
+        self.transportation_method = transportation_method.upper()
 
 
 class CreateTransportationMethodDTO:
-    def __init__(self, **kwargs):
-        self.transportation_method = kwargs.get("transportation_method")
+    def __init__(self, transportation_method):
+        error = self.validate(transportation_method)
+        if error:
+            raise Exception(error)
+        self.transportation_method = transportation_method.upper()
 
-    def validate(self):
+    def validate(self, transportation_method):
         error_list = []
-        if (
-            not self.transportation_method
-            or type(self.transportation_method) is not str
-        ):
+        if not transportation_method or type(transportation_method) is not str:
             error_list.append("The transportation_method supplied is not a string.")
+        return error_list

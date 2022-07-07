7ba47184164f9234d4f1333dd2fe7c8c568014bb
Julianne Jorda <58669961+juliannejorda@users.noreply.github.com>
2022-07-06 22:18:50 -0400

    Setup Basic Intake Form Flow (#43)
    Added next/prev step functions and case rendering

--- frontend/src/components/pages/IntakePage.tsx
+++ frontend/src/components/pages/IntakePage.tsx
@@ -1,12 +1,38 @@
-import React from "react";
-import { Text } from "@chakra-ui/react";
+import React, {useState} from "react";
+import { Button, Text } from "@chakra-ui/react";
 
 const Intake = (): React.ReactElement => {
-  return (
-    <div style={{ textAlign: "center", paddingTop: "20px" }}>
-      <Text textStyle="heading">Intake 💨</Text>
-    </div>
-  );
+  const [step, setStep] = useState(1);
+
+  const nextStep = () => setStep(step + 1);
+
+  const prevStep = () => setStep(step - 1);
+
+  switch(step) {
+    case 1:
+      return (
+        <div style={{ textAlign: "center", paddingTop: "20px" }}>
+          <Text textStyle="heading">Intake 💨 1 </Text>
+          <Button onClick={nextStep}>Next Button</Button>
+        </div>
+      );
+    case 2: 
+      return (
+        <div style={{ textAlign: "center", paddingTop: "20px" }}>
+          <Text textStyle="heading">Intake 💨 2</Text>
+          <Button onClick={prevStep}>Previous Button</Button>
+          <Button onClick={nextStep}>Next Button</Button>
+        </div>
+      );
+    default:
+      return (
+        <div style={{ textAlign: "center", paddingTop: "20px" }}>
+          <Text textStyle="heading">Intake 💨 3</Text>
+          <Button onClick={prevStep}>Previous Button</Button>
+        </div>
+      );
+  }
+  
 };
 
 export default Intake;

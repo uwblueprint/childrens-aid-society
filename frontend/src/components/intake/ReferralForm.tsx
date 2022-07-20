b79cbb5416a80cb395d5329addc9858d7d38bc91
juthikahoque <77185514+juthikahoque@users.noreply.github.com>
2022-07-19 21:32:33 -0400

    create dummy referrals screen (#49)
    * add referral forms screen
    
    * review changes and add chakra styling
    
    * rename font style import
    
    * update styling based on review
    
    * fix font styling
    
    * add font styling to package
    
    * alphabetize styling

---
+++ frontend/src/components/intake/ReferralForm.tsx
@@ -0,0 +1,150 @@
+import React from "react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  SimpleGrid,
+} from "@chakra-ui/react";
+
+export type ReferralDetails = {
+  cpinFileNumber: string;
+  cpinFileType: string;
+  familyName: string;
+  phoneNumber: string;
+  referralDate: string;
+  referringWorker: string;
+  referringWorkerContact: string;
+};
+
+type ReferralFormProps = {
+  referralDetails: ReferralDetails;
+  setReferralDetails: React.Dispatch<React.SetStateAction<ReferralDetails>>;
+};
+
+const ReferralForm = ({
+  referralDetails,
+  setReferralDetails,
+}: ReferralFormProps): React.ReactElement => {
+  return (
+    <Box style={{ padding: "0px 100px 30px 100px" }}>
+      <Heading textStyle="heading">Referral Details</Heading>
+      <FormControl style={{ padding: "30px" }}>
+        <SimpleGrid columns={2} spacing="70px">
+          <Box>
+            <FormLabel htmlFor="referringWorker" style={{ marginTop: "0px" }}>
+              REFERRING WORKER
+            </FormLabel>
+            <Input
+              value={referralDetails.referringWorker}
+              id="referringWorker"
+              type="string"
+              placeholder="Enter name of worker..."
+              onChange={(event) =>
+                setReferralDetails({
+                  ...referralDetails,
+                  referringWorker: event.currentTarget.value,
+                })
+              }
+            />
+          </Box>
+          <Box>
+            <FormLabel
+              htmlFor="referringWorkerContact"
+              style={{ marginTop: "0px" }}
+            >
+              REFERRING WORKER CONTACT
+            </FormLabel>
+            <Input
+              value={referralDetails.referringWorkerContact}
+              id="referringWorkerContact"
+              type="string"
+              placeholder="(ie. 223-2232-2323)"
+              onChange={(event) =>
+                setReferralDetails({
+                  ...referralDetails,
+                  referringWorkerContact: event.currentTarget.value,
+                })
+              }
+            />
+          </Box>
+        </SimpleGrid>
+        <FormLabel htmlFor="familyName">FAMILY NAME</FormLabel>
+        <Input
+          value={referralDetails.familyName}
+          id="familyName"
+          type="string"
+          placeholder="Enter family name..."
+          onChange={(event) =>
+            setReferralDetails({
+              ...referralDetails,
+              familyName: event.currentTarget.value,
+            })
+          }
+        />
+        <FormLabel htmlFor="referralDate">REFERRAL DATE</FormLabel>
+        {/* TO DO : change to date picker */}
+        <Input
+          value={referralDetails.referralDate}
+          id="referralDate"
+          type="string"
+          placeholder="Select a date..."
+          onChange={(event) =>
+            setReferralDetails({
+              ...referralDetails,
+              referralDate: event.currentTarget.value,
+            })
+          }
+        />
+        <SimpleGrid columns={2} spacing="70px">
+          <Box>
+            <FormLabel htmlFor="cpinFileNumber">CPIN FILE NUMBER</FormLabel>
+            <Input
+              value={referralDetails.cpinFileNumber}
+              id="cpinFileNumber"
+              type="string"
+              placeholder="(ie. 123456789)"
+              onChange={(event) =>
+                setReferralDetails({
+                  ...referralDetails,
+                  cpinFileNumber: event.currentTarget.value,
+                })
+              }
+            />
+          </Box>
+          <Box>
+            <FormLabel htmlFor="cpinFileType">CPIN FILE TYPE</FormLabel>
+            <Input
+              value={referralDetails.cpinFileType}
+              id="cpinFileType"
+              type="string"
+              placeholder="Select a file type..."
+              onChange={(event) =>
+                setReferralDetails({
+                  ...referralDetails,
+                  cpinFileType: event.currentTarget.value,
+                })
+              }
+            />
+          </Box>
+        </SimpleGrid>
+        <FormLabel htmlFor="phoneNumber">PHONE NUMBER</FormLabel>
+        <Input
+          value={referralDetails.phoneNumber}
+          id="phoneNumber"
+          type="string"
+          placeholder="(ie. 223-2232-2323)"
+          onChange={(event) =>
+            setReferralDetails({
+              ...referralDetails,
+              phoneNumber: event.currentTarget.value,
+            })
+          }
+        />
+      </FormControl>
+    </Box>
+  );
+};
+
+export default ReferralForm;
---
+++ frontend/src/theme/components.tsx
@@ -0,0 +1,59 @@
+import type { ComponentStyleConfig } from "@chakra-ui/theme";
+import textStyles from "./textStyles";
+// TODO: separate out each component into different files
+
+// Button styling
+export const Button: ComponentStyleConfig = {
+  baseStyle: {
+    textTransform: "uppercase",
+    borderRadius: "none",
+  },
+  variants: {
+    solid: {
+      _hover: {
+        background: "gray.800",
+      },
+      background: "#000000",
+      border: "none",
+      color: "white",
+      fontSize: "14px",
+      fontWeight: "600",
+      lineHeight: "16px",
+      opacity: "0.5",
+      padding: "21px 118px",
+    },
+  },
+  defaultProps: {
+    variant: "solid",
+  },
+};
+
+export const FormLabel: ComponentStyleConfig = {
+  baseStyle: {
+    paddingTop: "15px",
+  },
+  variants: {
+    formLabel: textStyles.formLabel,
+  },
+  defaultProps: {
+    variant: "formLabel",
+  },
+};
+
+export const Input: ComponentStyleConfig = {
+  variants: {
+    solid: {
+      field: {
+        background: "#EEECF2",
+        borderRadius: "0px",
+        _placeholder: {
+          color: "#888897",
+          padding: "10px 0px",
+        },
+      },
+    },
+  },
+  defaultProps: {
+    variant: "solid",
+  },
+};

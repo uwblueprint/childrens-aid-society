f20a2b617c9edefd888ebf934e198f595a978d2b
Hamza Yusuff <63330003+hamza-yusuff@users.noreply.github.com>
2022-07-19 21:23:15 -0400

    Update caregiver service tests

--- backend/python/app/resources/caregiver_dto.py
+++ backend/python/app/resources/caregiver_dto.py
@@ -1,11 +1,30 @@
 import datetime
 
+
 class CaregiverDTO:
-    def __init__(self, id, type, first_name, last_name, is_primary,
-                child_id, address_id, relationship_to_child, phone_number,
-                cpin_number, date_of_birth, special_needs, name_of_child,
-                kinship_worker_name, kinship_worker_ext, foster_care_coord_name,
-                foster_care_coord_ext, limitations_for_access, address, child):
+    def __init__(
+        self,
+        id,
+        type,
+        first_name,
+        last_name,
+        is_primary,
+        child_id,
+        address_id,
+        relationship_to_child,
+        phone_number,
+        cpin_number,
+        date_of_birth,
+        special_needs,
+        name_of_child,
+        kinship_worker_name,
+        kinship_worker_ext,
+        foster_care_coord_name,
+        foster_care_coord_ext,
+        limitations_for_access,
+        address,
+        child,
+    ):
 
         self.id = id
         self.type = type
@@ -58,32 +77,28 @@ class CreateCaregiverDTO:
             error_list.append("The last name value supplied is not a string.")
         if type(self.last_name) is str and self.last_name == "":
             error_list.append("The last name value supplied is empty.")
-        if type(self.is_primary) is not bool:
-            error_list.append("The is_primary field is not of boolean type.")
-        if type(self.child_id) is not int:
+        if self.child_id and type(self.child_id) is not int:
             error_list.append("The child id field is not of integer type.")
-        if type(self.address_id) is not int:
+        if self.address_id and type(self.address_id) is not int:
             error_list.append("The address id field is not of integer type.")
         if type(self.phone_number) is not str:
             error_list.append("The phone number value is not a string.")
         if type(self.phone_number) is str and self.phone_number == "":
             error_list.append("Phone number cannot be empty.")
-        if type(self.cpin_number) is not str:
+        if self.cpin_number and type(self.cpin_number) is not str:
             error_list.append("The cpin number is not a string.")
-        if type(self.date_of_birth) is not datetime.datetime():
-            error_list.append("The date of birth provided is not of DATE format.")
-        if type(self.special_needs) is not str:
+        if self.special_needs and type(self.special_needs) is not str:
             error_list.append("The special needs value is not string.")
-        if type(self.name_of_child) is not str:
+        if self.name_of_child and type(self.name_of_child) is not str:
             error_list.append("The name of child value is not string.")
-        if type(self.kinship_worker_name) is not str:
+        if self.kinship_worker_name and type(self.kinship_worker_name) is not str:
             error_list.append("The kinship worker name value is not string.")
-        if type(self.kinship_worker_ext) is not str:
+        if self.kinship_worker_ext and type(self.kinship_worker_ext) is not str:
             error_list.append("The kinship worker ext value is not string.")
-        if type(self.foster_care_coord_name) is not str:
+        if self.foster_care_coord_name and type(self.foster_care_coord_name) is not str:
             error_list.append("The foster care coord name value is not string")
-        if type(self.limitations_for_access) is not str:
-            error_list.append("The limitations for access value supplied is not string.")
+        if self.limitations_for_access and type(self.limitations_for_access) is not str:
+            error_list.append(
+                "The limitations for access value supplied is not string."
+            )
         return error_list
-
-
---
+++ backend/python/tests/functional/test_caregiver_service.py
@@ -0,0 +1,115 @@
+import datetime
+
+import pytest
+from flask import current_app
+
+from app.models import db
+from app.models.address import Address
+from app.models.caregiver import Caregiver
+from app.models.child import Child
+from app.models.daytime_contact import DaytimeContact
+from app.models.user import User
+from app.resources.caregiver_dto import CaregiverDTO, CreateCaregiverDTO
+from app.services.implementations.caregiver_service import CaregiverService
+
+
+@pytest.fixture
+def caregiver_service():
+    caregiver = CaregiverService(current_app.logger)
+    seed_database()
+    yield caregiver
+    # User.query.delete()
+    # Address.query.delete()
+    # DaytimeContact.query.delete()
+    # Child.query.delete()
+    Caregiver.query.delete()
+
+
+DUMMY_USER_DATA = {
+    "id": 1,
+    "first_name": "Hamza",
+    "last_name": "Yusuff",
+    "auth_id": "hbyusuff",
+    "role": "Driver",
+    "branch": "ALGOMA",
+}
+
+DUMMY_ADDRESS_DATA = {
+    "id": 1,
+    "street_address": "Lester Street",
+    "city": "waterloo",
+    "postal_code": "N2L3W6",
+}
+
+DUMMY_DAYTIME_CONTACT_DATA = {
+    "id": 1,
+    "contact_first_name": "Hamza",
+    "contact_last_name": "Yusuff",
+    "address_id": 1,
+    "phone_number": "8790832",
+    "type": "SCHOOL",
+}
+
+DUMMY_CHILD_DATA = {
+    "first_name": "Hamza",
+    "last_name": "Yusuff",
+    "child_service_worker_id": 1,
+    "daytime_contact_id": 1,
+    "special_needs": "None",
+    "has_kinship_provider": False,
+    "has_foster_placement": False,
+}
+
+
+def seed_database():
+    dummy_method_instances = [
+        Child(**DUMMY_CHILD_DATA),
+        Address(**DUMMY_ADDRESS_DATA),
+        DaytimeContact(**DUMMY_DAYTIME_CONTACT_DATA),
+    ]
+    for dummy in dummy_method_instances:
+        db.session.add(dummy)
+    db.session.commit()
+
+
+class TestCreateCaregiverSuccess:
+    def test_nullable_true(self, caregiver_service):
+        param = CreateCaregiverDTO(
+            type="PROVIDER",
+            first_name="Hamza",
+            last_name="Yusuff",
+            child_id=1,
+            relationship_to_child="DAD",
+            phone_number="980332434",
+        )
+        caregiver_instance = caregiver_service.create_caregiver(param)
+        assert type(caregiver_instance) is CaregiverDTO
+
+
+class TestCreateCaregiverInvalidFails:
+    def test_missing_field(self, caregiver_service):
+        param = CreateCaregiverDTO(
+            type="PROVIDER",
+            first_name="Hamza",
+            child_id=1,
+            relationship_to_child="DAD",
+            phone_number="980332434",
+        )
+        with pytest.raises(Exception):
+            caregiver_service.create_caregiver(param)
+
+    def test_empty_input_string(self):
+        param = CreateCaregiverDTO(
+            type="PROVIDER",
+            first_name="Hamza",
+            last_name="",
+            child_id=1,
+            relationship_to_child="DAD",
+            phone_number="980332434",
+        )
+        with pytest.raises(Exception):
+            caregiver_service.create_caregiver(param)
+
+    def test_empty_param(self):
+        with pytest.raises(Exception):
+            caregiver_service.create_caregiver(None)

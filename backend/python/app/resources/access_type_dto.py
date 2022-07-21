0f8524f792b88c92e9ec6868736cf1d8c56a38ce
Atharva Gangal <70596721+thrvg@users.noreply.github.com>
2022-07-20 20:25:32 -0400

    Separate get add methods and maintain consistency (#48)
    * Separate get add methods and maintain consistency
    
    * Resolve errors
    
    * Ran linter

--- backend/python/app/resources/access_type_dto.py
+++ backend/python/app/resources/access_type_dto.py
@@ -2,16 +2,3 @@ class AccessTypeDTO:
     def __init__(self, id, access_type):
         self.id = id
         self.access_type = access_type
-
-
-class CreateAccessTypeDTO:
-    def __init__(self, **kwargs):
-        self.access_type = kwargs.get("access_type")
-
-    def validate(self):
-        error_list = []
-        if type(self.access_type) is not str:
-            error_list.append("The access_type value supplied is not a string.")
-        if self.access_type == "":
-            error_list.append("The access_type value supplied is empty.")
-        return error_list

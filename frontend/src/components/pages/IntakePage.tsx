fcb6c9e0da9feee3e2e80cd29e51af661e7c6477
juthikahoque <juthika1@yahoo.com>
2022-07-24 12:09:28 -0400

    add functionality to add button

--- frontend/src/components/pages/IntakePage.tsx
+++ frontend/src/components/pages/IntakePage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
-import { Box, Button, Heading } from "@chakra-ui/react";
+import { Box, Button, Heading, others } from "@chakra-ui/react";
 import ReferralForm, { ReferralDetails } from "../intake/ReferralForm";
+import PermittedIndividualsForm, { PermittedIndividualDetails } from "../intake/PermittedIndividualsForm";
 
 const Intake = (): React.ReactElement => {
   const [step, setStep] = useState(1);
@@ -14,9 +15,50 @@ const Intake = (): React.ReactElement => {
     phoneNumber: "",
   });
 
+  const [
+    permittedIndividualDetails,
+    setPermittedIndividualDetails,
+  ] = useState<PermittedIndividualDetails>({
+    firstName: "",
+    lastName: "",
+    relationship: "",
+    phoneNumber: "",
+  });
+
+  const [
+    allOtherPermittedIndividuals,
+    setAllOtherPermittedIndividuals,
+  ] = useState<PermittedIndividualDetails[]>([]);
+
+  const [otherScreens, setOtherScreens] = useState<React.ReactElement[]>([]);
+
+  // TODO: make so that previous and next auto saves the allOtherPermittedIndividuals
   const nextStep = () => setStep(step + 1);
 
   const prevStep = () => setStep(step - 1);
+
+  // TODO: make it so that it adds new components on same page
+  const addOtherIndividuals = () => {
+    setAllOtherPermittedIndividuals([
+      ...allOtherPermittedIndividuals,
+      permittedIndividualDetails,
+    ]);
+    setOtherScreens([
+      ...otherScreens,
+      <PermittedIndividualsForm
+        key={otherScreens.length}
+        permittedIndividualDetails={permittedIndividualDetails}
+        setPermittedIndividualDetails={setPermittedIndividualDetails}
+      />,
+    ]);
+    setPermittedIndividualDetails({
+      firstName: "",
+      lastName: "",
+      phoneNumber: "",
+      relationship: "",
+    });
+    nextStep();
+  };
 
   switch (step) {
     case 1:
@@ -39,9 +81,15 @@ const Intake = (): React.ReactElement => {
       );
     default:
       return (
-        <Box style={{ textAlign: "center", padding: "20px 0px 20px 0px" }}>
-          <Heading textStyle="heading">Intake 💨 3</Heading>
+        <Box style={{ textAlign: "center", padding: "30px 0px 40px 0px" }}>
+          <PermittedIndividualsForm
+            key={otherScreens.length - 1}
+            permittedIndividualDetails={permittedIndividualDetails}
+            setPermittedIndividualDetails={setPermittedIndividualDetails}
+          />
+          <Button onClick={addOtherIndividuals}>Add</Button>
           <Button onClick={prevStep}>Previous Button</Button>
+          <Button onClick={nextStep}>Next Button</Button>
         </Box>
       );
   }

72c233d1fe6cce14adc0cf197ba26dade83ea5bb
Hamza Yusuff <63330003+hamza-yusuff@users.noreply.github.com>
2022-08-30 00:00:42 -0400

    Added current datetime as default

--- backend/python/app/models/goal.py
+++ backend/python/app/models/goal.py
@@ -1,3 +1,5 @@
+import datetime
+
 from sqlalchemy import text
 
 from . import db
@@ -12,8 +14,16 @@ class Goal(db.Model, BaseMixin):
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     type = db.Column(type_enum, nullable=False)
     goal = db.Column(db.String, nullable=False)
-    start_date = db.Column(db.Date, nullable=True)
-    end_date = db.Column(db.Date, nullable=True)
+    start_date = db.Column(
+        db.Date,
+        default=datetime.datetime.today(),
+        nullable=True,
+    )
+    end_date = db.Column(
+        db.Date,
+        default=datetime.datetime.today(),
+        nullable=True,
+    )
     is_default = db.Column(
         db.Boolean, nullable=False, default=False, server_default=text("False")
     )

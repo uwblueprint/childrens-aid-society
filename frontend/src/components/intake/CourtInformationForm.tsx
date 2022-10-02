c79193c3211aecaa0a06c5b516d3610fe6d071dd
Kabir Oberai <oberai.kabir@gmail.com>
2022-10-01 21:57:18 -0400

    hifi-ify court info

--- frontend/src/components/intake/CourtInformationForm.tsx
+++ frontend/src/components/intake/CourtInformationForm.tsx
@@ -2,14 +2,18 @@ import React from "react";
 import {
   Box,
   Button,
+  Container,
   FormControl,
   FormLabel,
   Heading,
+  HStack,
   Input,
   Select,
 } from "@chakra-ui/react";
 import { Field, Form, Formik } from "formik";
+import { FilePlus } from "react-feather";
 import { AutocompleteField } from "./Autocomplete";
+import CustomInput from "../common/CustomInput";
 
 export type CourtDetails = {
   currentCourtStatus: string;
@@ -24,6 +28,12 @@ type CourtInformationFormProps = {
   nextStep: () => void;
   prevStep: () => void;
 };
+
+const optional = (
+  <Box display="inline" color="gray.600">
+    (OPTIONAL)
+  </Box>
+);
 
 const CourtInformationForm = ({
   courtDetails,
@@ -51,32 +61,32 @@ const CourtInformationForm = ({
 
   return (
     <Formik initialValues={courtDetails} onSubmit={onSubmit}>
-      {({ handleSubmit, setFieldValue }) => {
+      {({ handleSubmit, setFieldValue, values }) => {
         return (
-          <Form style={{ padding: "0px 100px 30px 100px" }}>
-            <Heading textStyle="heading">Court Details</Heading>
-            <Box style={{ padding: "30px" }}>
-              <FormLabel pt="15px" htmlFor="currentCourtStatus">
-                Current Court Status
-              </FormLabel>
-              <AutocompleteField
-                id="currentCourtStatus"
-                name="currentCourtStatus"
-                placeholder="Enter or select a court status"
-                hints={[
-                  "Interim care",
-                  "Final order for Society Care",
-                  "Extended Society Care",
-                  "Supervision order",
-                  "Kin service placement",
-                  "Living with Biological family",
-                ]}
-              />
-            </Box>
-            <FormControl>
+          <Container maxWidth="container.xl">
+            <Form style={{ padding: "0 0 30px 0" }}>
+              <Heading textStyle="heading">Court Details</Heading>
+              <Box style={{ padding: "16px 0" }}>
+                <FormLabel pt="15px" htmlFor="currentCourtStatus">
+                  COURT STATUS
+                </FormLabel>
+                <AutocompleteField
+                  id="currentCourtStatus"
+                  name="currentCourtStatus"
+                  placeholder="Enter or select a court status"
+                  hints={[
+                    "Interim care",
+                    "Final order for Society Care",
+                    "Extended Society Care",
+                    "Supervision order",
+                    "Kin service placement",
+                    "Living with Biological family",
+                  ]}
+                />
+              </Box>
               {/* TODO: store the uploaded file and save in backend */}
               <Input
-                style={{ display: "none" }}
+                display="none"
                 type="file"
                 ref={inputRef}
                 onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
@@ -85,53 +95,96 @@ const CourtInformationForm = ({
                 id="orderReferral"
                 name="orderReferral"
               />
-              <Button onClick={handleClick}>Attach Order Referral</Button>
-            </FormControl>
-            <FormControl style={{ padding: "30px" }}>
-              <FormLabel pt="15px" htmlFor="firstNationHeritage">
-                First Nation Heritage
-              </FormLabel>
-              <Field
-                id="firstNationHeritage"
-                as={Select}
-                name="firstNationHeritage"
-                placeholder="Select Heritage..."
+              <FormControl padding="16px 0">
+                <FormLabel
+                  pt="15px"
+                  htmlFor="documentDisplay"
+                  style={{ cursor: "pointer" }}
+                >
+                  ORDER REFERRAL
+                </FormLabel>
+                <HStack>
+                  <Field
+                    as={CustomInput}
+                    isReadOnly
+                    id="documentDisplay"
+                    placeholder="No document attached"
+                    value={values.orderReferral?.name}
+                    onClick={handleClick}
+                    marginRight="10px"
+                    style={{ cursor: "pointer" }}
+                  />
+                  <Button
+                    onClick={handleClick}
+                    variant="ghost"
+                    padding="24px 20px"
+                    color="blue.400"
+                    leftIcon={<FilePlus />}
+                  >
+                    Attach document
+                  </Button>
+                </HStack>
+              </FormControl>
+              <HStack alignItems="end" padding="16px 0" spacing={10}>
+                <FormControl>
+                  <FormLabel pt="15px" htmlFor="firstNationHeritage">
+                    FIRST NATION HERITAGE {optional}
+                  </FormLabel>
+                  <Field
+                    id="firstNationHeritage"
+                    as={Select}
+                    name="firstNationHeritage"
+                    variant="filled"
+                    height="48px"
+                    backgroundColor="gray.50"
+                    borderColor="gray.100"
+                    borderWidth="1px"
+                    fontSize="18px"
+                    color={values.firstNationHeritage ? "black" : "gray.600"}
+                    sx={{ ":hover, :focus": { backgroundColor: "gray.100" } }}
+                  >
+                    <option value="" selected>
+                      Select First Nation heritage status
+                    </option>
+                    <option>First Nation Registered</option>
+                    <option>Eligible for Registration</option>
+                    <option>Inuit</option>
+                    <option>Metis</option>
+                    <option>Unknown</option>
+                  </Field>
+                </FormControl>
+                <FormControl>
+                  <FormLabel pt="15px" htmlFor="firstNationBand">
+                    FIRST NATION BAND {optional}
+                  </FormLabel>
+                  <Field
+                    as={CustomInput}
+                    id="firstNationBand"
+                    placeholder="Enter First Nation Band"
+                    name="firstNationBand"
+                  />
+                </FormControl>
+              </HStack>
+              <Button
+                onClick={() => {
+                  handleSubmit();
+                  prevStep();
+                }}
+                marginRight="10px"
               >
-                <option>United Arab Emirates</option>
-                <option>Nigeria</option>
-              </Field>
-            </FormControl>
-            <FormControl style={{ padding: "30px" }}>
-              <FormLabel pt="15px" htmlFor="firstNationBand">
-                First Nation Band
-              </FormLabel>
-              <Field
-                as={Select}
-                id="firstNationBand"
-                placeholder="Select Band..."
-                name="firstNationBand"
+                Previous Button
+              </Button>
+              <Button
+                onClick={() => {
+                  handleSubmit();
+                  nextStep();
+                }}
+                marginLeft="10px"
               >
-                <option>United Arab Emirates</option>
-                <option>Nigeria</option>
-              </Field>
-            </FormControl>
-            <Button
-              onClick={() => {
-                handleSubmit();
-                prevStep();
-              }}
-            >
-              Previous Button
-            </Button>
-            <Button
-              onClick={() => {
-                handleSubmit();
-                nextStep();
-              }}
-            >
-              Next Button
-            </Button>
-          </Form>
+                Next Button
+              </Button>
+            </Form>
+          </Container>
         );
       }}
     </Formik>

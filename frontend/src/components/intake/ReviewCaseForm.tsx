e1f0fb91a2707095b63120a1b83b5c770be8b380
Julianne Jorda <julianne.jorda@yahoo.com>
2022-10-25 20:06:06 -0400

    Changed to Box

--- frontend/src/components/intake/ReviewCaseForm.tsx
+++ frontend/src/components/intake/ReviewCaseForm.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { Button, FormControl, Heading } from "@chakra-ui/react";
-import { Form, Formik } from "formik";
+import { Box } from "@chakra-ui/react";
 
 type ReviewFormProps = {
   prevStep: () => void;
@@ -9,24 +8,7 @@ type ReviewFormProps = {
 const ReviewForm = ({ prevStep }: ReviewFormProps): React.ReactElement => {
   const onSubmit = () => {};
 
-  return (
-    <Formik initialValues={{ reviewDetails: "test" }} onSubmit={onSubmit}>
-      {({ handleSubmit }) => (
-        <Form style={{ padding: "0px 100px 30px 100px" }}>
-          <Heading textStyle="header-large">Review Case Details</Heading>
-          <FormControl style={{ padding: "30px" }}>Basic Form</FormControl>
-          <Button
-            onClick={() => {
-              handleSubmit();
-              prevStep();
-            }}
-          >
-            Previous Button
-          </Button>
-        </Form>
-      )}
-    </Formik>
-  );
+  return <Box>Review case details</Box>;
 };
 
 export default ReviewForm;

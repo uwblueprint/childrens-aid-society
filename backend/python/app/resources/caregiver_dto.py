b06901e4790de310cd6e6bbae5beb755992218f5
Kai Wei Mo <devinkwmo@gmail.com>
2022-10-25 23:49:17 -0400

    Fix: secondary phone number null

--- backend/python/app/resources/caregiver_dto.py
+++ backend/python/app/resources/caregiver_dto.py
@@ -80,15 +80,16 @@ class CreateCaregiverDTO:
         else:
             error_list.append("primary_phone_number must be a string")
 
-        if type(self.secondary_phone_number) is str:
-            if len(self.secondary_phone_number) == 0:
-                error_list.append("secondary_phone_number must not be empty")
-            # if not re.match(r"^\d{3}-\d{3}-\d{4}$", self.secondary_phone_number):
-            #     error_list.append(
-            #         "secondary_phone_number must be in the format XXX-XXX-XXXX"
-            #     )
-        else:
-            error_list.append("secondary_phone_number must be a string")
+        if self.secondary_phone_number is not None:
+            if type(self.secondary_phone_number) is str:
+                if len(self.secondary_phone_number) == 0:
+                    error_list.append("secondary_phone_number must not be empty")
+                # if not re.match(r"^\d{3}-\d{3}-\d{4}$", self.secondary_phone_number):
+                #     error_list.append(
+                #         "secondary_phone_number must be in the format XXX-XXX-XXXX"
+                #     )
+            else:
+                error_list.append("secondary_phone_number must be a string")
 
         if type(self.email) is str:
             if not re.match(

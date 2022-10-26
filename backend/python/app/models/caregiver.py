db59451c05133857fd17e5828f58a9566a6118b1
Kai Wei Mo <devinkwmo@gmail.com>
2022-10-25 22:07:06 -0400

    Update models

--- backend/python/app/models/caregiver.py
+++ backend/python/app/models/caregiver.py
@@ -1,7 +1,6 @@
 from . import db
 from .base_mixin import BaseMixin
 
-caregiver_type_enum = db.Enum("CAREGIVER", "PROVIDER", name="caregivers_type")
 relationship_to_child_enum = db.Enum(
     "FOSTER_CAREGIVER",
     "KINSHIP_CAREGIVER",
@@ -14,19 +13,14 @@ class Caregiver(db.Model, BaseMixin):
     __tablename__ = "caregivers"
 
     # TODO: add foreign key reference to child
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
-    type = db.Column(caregiver_type_enum, nullable=False)
-    first_name = db.Column(db.String, nullable=False)
-    last_name = db.Column(db.String, nullable=False)
-    date_of_birth = db.Column(db.Date, nullable=True)
-    special_needs = db.Column(db.String, nullable=True)
-    file_number = db.Column(db.String, nullable=False)
-    primary_phone = db.Column(db.String, nullable=False)
-    secondary_phone = db.Column(db.String, nullable=True)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    date_of_birth = db.Column(db.Date, nullable=False)
+    individual_considerations = db.Column(db.String, nullable=True)
+    primary_phone_number = db.Column(db.String, nullable=False)
+    secondary_phone_number = db.Column(db.String, nullable=True)
     email = db.Column(db.String, nullable=False)
-    address_id = db.Column(db.Integer, db.ForeignKey("addresses.id"), nullable=False)
-    address = db.relationship("Address", backref="caregivers")
+    address = db.Column(db.String, nullable=False)
     relationship_to_child = db.Column(relationship_to_child_enum, nullable=False)
-    preferred_contact_method = db.Column(db.String, nullable=True)
+    additional_contact_notes = db.Column(db.String, nullable=True)
     intake_id = db.Column(db.Integer, db.ForeignKey("intakes.id"), nullable=False)
-    intake = db.relationship("Intake", backref="caregivers")
--- backend/python/app/models/provider.py
+++ backend/python/app/models/provider.py
@@ -1,17 +1,17 @@
 from . import db
 from .base_mixin import BaseMixin
 
+
 class Provider(db.Model, BaseMixin):
     __tablename__ = "providers"
 
-    id = db.Column(db.Integer, primary_key=True, nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     file_number = db.Column(db.String, nullable=False)
     primary_phone_number = db.Column(db.String, nullable=False)
     secondary_phone_number = db.Column(db.String, nullable=True)
-    email = db.Column(db.String, nullable=False)
-    address_id = db.Column(db.Integer, db.ForeignKey("addresses.id"), nullable=False)
-    address = db.relationship("Address", backref="providers")
+    email = db.Column(db.String, nullable=True)
+    address = db.Column(db.String, nullable=False)
     relationship_to_child = db.Column(db.String, nullable=False)
-    preferred_contact_method = db.Column(db.String, nullable=True)
+    additional_contact_notes = db.Column(db.String, nullable=True)
     child_id = db.Column(db.Integer, db.ForeignKey("children.id"), nullable=False)

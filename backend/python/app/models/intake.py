4b2d3690a5c6a6281f87d30f777303947da46cee
Kai Wei Mo <devinkwmo@gmail.com>
2022-11-02 00:12:55 -0400

    initial commit

--- backend/python/app/models/intake.py
+++ backend/python/app/models/intake.py
@@ -3,6 +3,20 @@ import sqlalchemy.dialects.postgresql as pg
 from . import db
 from .base_mixin import BaseMixin
 
+intake_status_enum = pg.ENUM(
+    "IN PROGRESS",
+    "IN REVIEW",
+    "ACCEPTED",
+    "DENIED",
+    name="intake_status",
+)
+
+cpin_file_type_enum = pg.ENUM(
+    "INVESTIGATION",
+    "ONGOING",
+    name="cpin_file_type",
+)
+
 court_status_enum = db.Enum(
     "INTERIM_CARE",
     "FINAL_ORDER_FOR_SOCIETY_CARE",
@@ -13,6 +27,7 @@ court_status_enum = db.Enum(
     "OTHER",
     name="intakes_court_status",
 )
+
 
 first_nation_heritage_enum = db.Enum(
     "FIRST_NATION_REGISTERED",
@@ -34,50 +49,62 @@ intakes_access_weekday_enum = db.Enum(
     name="intakes_access_weekday",
 )
 
-intakes_concerns = db.Table(
-    "intakes_concerns",
-    db.metadata,
-    db.Column("intake_id", db.ForeignKey("intakes.id")),
-    db.Column("concern_id", db.ForeignKey("concerns.id")),
-)
-
-intakes_goals = db.Table(
-    "intakes_goals",
-    db.metadata,
-    db.Column("intake_id", db.ForeignKey("intakes.id")),
-    db.Column("goal_id", db.ForeignKey("goals.id")),
-    db.Column("start_date", db.DateTime),
-    db.Column("end_date", db.DateTime),
-)
-
 
 class Intake(db.Model, BaseMixin):
     __tablename__ = "intakes"
 
+    """
+        id [unchanged]
+        user_id
+        intake_status (enum 'IN PROGRESS', 'IN REVIEW', 'ACCEPTED', 'DENIED'; default 'IN PROGRESS')
+        referring_worker_name (string)
+        referring_worker_contact (string)
+        referral_date [unchanged]
+        family_name [unchanged]
+        cpin_number [unchanged]
+        cpin_file_type (enum 'INVESTIGATION' or 'ONGOING')
+    """
     id = db.Column(db.Integer, primary_key=True, nullable=False)
-    referring_worker_id = db.Column(
-        db.Integer, db.ForeignKey("users.id"), nullable=True
-    )
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=True)
+    intake_status = db.Column(intake_status_enum, nullable=True, default="IN PROGRESS")
+    referring_worker_name = db.Column(db.String, nullable=False)
+    referring_worker_contact = db.Column(db.String, nullable=False)
     referral_date = db.Column(db.Date, nullable=True)
     family_name = db.Column(db.String, nullable=True)
     cpin_number = db.Column(db.String, nullable=True)
-    is_investigation = db.Column(db.Boolean, nullable=True)
-    is_ongoing = db.Column(db.Boolean, nullable=True)
-    is_court_involved = db.Column(db.Boolean, nullable=True)
+    cpin_file_type = db.Column(
+        cpin_file_type_enum, nullable=False, default="INVESTIGATION"
+    )
+
+    """
+        court_status [unchanged]
+        court_order_file [unchanged]
+        first_nation_heritage [unchanged]
+        first_nation_band [unchanged]
+    """
     court_status = db.Column(court_status_enum, nullable=True)
-    court_order = db.Column(db.String, nullable=True)
     court_order_file = db.Column(db.String, nullable=True)
-    is_first_nation_heritage = db.Column(db.Boolean, nullable=True)
     first_nation_heritage = db.Column(first_nation_heritage_enum, nullable=True)
     first_nation_band = db.Column(db.String, nullable=True)
-    family_strengths = db.Column(db.String, nullable=True)
-    access_type = db.Column(db.String, nullable=True)
-    transportation = db.Column(db.String, nullable=True)
-    limitations = db.Column(db.String, nullable=True)
-    case_date = db.Column(db.Date, nullable=True)
-    is_accepted = db.Column(db.Boolean, nullable=True)
+
+    """
+        transportation_method (string)
+        access_type (string)
+        access_start_date (date)
+    """
+    transportation_method = db.Column(db.String, nullable=False)
+    access_type = db.Column(db.String, nullable=False)
+    access_start_date = db.Column(db.Date, nullable=False)
+
+    """
+        date_accepted [unchanged] (nullable)
+        access_weekday [unchanged]
+        access_location_id [unchanged]
+        access_time [unchanged]
+        lead_access_worker_id [unchanged] (nullable)
+        denial_reason [unchanged] (nullable)
+    """
     date_accepted = db.Column(db.Date, nullable=True)
-    access_start_date = db.Column(db.Date, nullable=True)
     access_weekday = db.Column(pg.ARRAY(intakes_access_weekday_enum), nullable=True)
     access_location_id = db.Column(
         db.Integer, db.ForeignKey("addresses.id"), nullable=True
@@ -87,8 +114,3 @@ class Intake(db.Model, BaseMixin):
         db.Integer, db.ForeignKey("users.id"), nullable=True
     )
     denial_reason = db.Column(db.String, nullable=True)
-    access_location = db.relationship("Address")
-    referring_worker = db.relationship("User", foreign_keys=[referring_worker_id])
-    lead_access_worker = db.relationship("User", foreign_keys=[lead_access_worker_id])
-    concerns = db.relationship("Concern", secondary=intakes_concerns)
-    goals = db.relationship("Goal", secondary=intakes_goals)

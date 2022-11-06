556b775bcdda6451ec0c326ceda400a19b11c68e
Het7 <hetparikh7@gmail.com>
2022-11-06 11:09:57 -0500

    Yarn fix

--- frontend/src/components/intake/IntakeFormFooter.tsx
+++ frontend/src/components/intake/IntakeFormFooter.tsx
@@ -1,19 +1,19 @@
 import React from "react";
-import { Box, Button, Heading, Text } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
-export type IntakeFooterProps = {
-};
+export type IntakeFooterProps = Record<string, never>;
 
-const IntakeFooter = (
-
- ): React.ReactElement => {
+const IntakeFooter = (): React.ReactElement => {
   return (
     <Box bg="gray.50" h="90px">
-      <Button type="submit" ml= "1200px" mt="25px">Next Section</Button>
-      <Button variant="tertiary" ml= "1050px" mt="-65px">Clear page</Button>
+      <Button type="submit" ml="1200px" mt="25px">
+        Next Section
+      </Button>
+      <Button variant="tertiary" ml="1050px" mt="-65px">
+        Clear page
+      </Button>
     </Box>
   );
 };
-
 
 export default IntakeFooter;

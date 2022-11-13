4668dcc25e81f5929ff97534dc1d86d3d2eab6a3
Kabir Oberai <oberai.kabir@gmail.com>
2022-11-13 12:33:23 -0500

    Update Select solid variant

--- frontend/src/components/intake/Select.tsx
+++ frontend/src/components/intake/Select.tsx
@@ -19,14 +19,7 @@ export const CustomSelectField = ({
     <Select
       {...{ ...field, ...props }}
       name={name}
-      variant="filled"
-      height="48px"
-      backgroundColor="gray.50"
-      borderColor="gray.100"
-      borderWidth="1px"
-      fontSize="18px"
       color={field.value ? "black" : "gray.600"}
-      sx={{ ":hover, :focus": { backgroundColor: "gray.100" } }}
     >
       {children}
     </Select>

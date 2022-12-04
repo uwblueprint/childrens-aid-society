ec3ccb5e431dd0f964246741d36a749480f1cf52
Andrew Ma <ayma01@gmail.com>
2022-12-04 09:31:59 -0800

    Create add school/daycare form

--- frontend/src/components/intake/child-information/AddChildPage.tsx
+++ frontend/src/components/intake/child-information/AddChildPage.tsx
@@ -6,7 +6,7 @@ import IntakeHeader from "../IntakeHeader";
 import ChildInformationForm, { ChildDetails } from "./ChildInformationForm";
 import ChildProviderForm from "./ChildProviderForm";
 import FormSelector from "./FormSelector";
-import SchoolDaycareForm from "./SchoolDaycareForm";
+import SchoolDaycareForm, { SchoolDaycareDetails } from "./SchoolDaycareForm";
 
 const AddChild = (): React.ReactElement => {
   const [activeFormIndex, setActiveFormIndex] = useState(0);
@@ -19,6 +19,13 @@ const AddChild = (): React.ReactElement => {
     specialNeeds: "",
     childBehaviours: "",
   });
+  const [schoolDaycareDetails, setSchoolDaycareDetails] =
+    useState<SchoolDaycareDetails>({
+      schoolName: "",
+      phoneNumber: "",
+      address: "",
+      dismissalTime: "",
+    });
 
   const renderChildForm = () => {
     switch (activeFormIndex) {
@@ -30,7 +37,12 @@ const AddChild = (): React.ReactElement => {
           />
         );
       case 1:
-        return <SchoolDaycareForm />;
+        return (
+          <SchoolDaycareForm
+            schoolDaycareDetails={schoolDaycareDetails}
+            setSchoolDaycareDetails={setSchoolDaycareDetails}
+          />
+        );
       case 2:
         return <ChildProviderForm />;
       default:
--- frontend/src/components/intake/child-information/SchoolDaycareForm.tsx
+++ frontend/src/components/intake/child-information/SchoolDaycareForm.tsx
@@ -1,11 +1,94 @@
-import { Box, Text } from "@chakra-ui/react";
 import React from "react";
+import {
+  Box,
+  FormControl,
+  FormLabel,
+  Icon,
+  SimpleGrid,
+} from "@chakra-ui/react";
+import { Field, Form, Formik } from "formik";
+import { Home, Phone, Navigation, Clock } from "react-feather";
+import CustomInput from "../../common/CustomInput";
 
-const SchoolDaycareForm = (): React.ReactElement => {
+export type SchoolDaycareDetails = {
+  schoolName: string;
+  phoneNumber: string;
+  address: string;
+  dismissalTime: string;
+};
+
+type SchoolDaycareFormProps = {
+  schoolDaycareDetails: SchoolDaycareDetails;
+  setSchoolDaycareDetails: React.Dispatch<
+    React.SetStateAction<SchoolDaycareDetails>
+  >;
+};
+
+const SchoolDaycareForm = ({
+  schoolDaycareDetails,
+  setSchoolDaycareDetails,
+}: SchoolDaycareFormProps): React.ReactElement => {
+  const onSubmit = (values: SchoolDaycareDetails) => {
+    setSchoolDaycareDetails(values);
+  };
+
   return (
-    <Box>
-      <Text>School / Daycare </Text>
-    </Box>
+    <Formik initialValues={schoolDaycareDetails} onSubmit={onSubmit}>
+      <Form style={{ padding: "2rem 12rem" }}>
+        <FormControl style={{ padding: "30px" }}>
+          <SimpleGrid columns={2} spacingX="3rem" spacingY="0.75rem">
+            <Box>
+              <FormLabel htmlFor="schoolName">NAME</FormLabel>
+              <Field
+                as={CustomInput}
+                id="schoolName"
+                name="schoolName"
+                type="string"
+                placeholder="Public School Name"
+                icon={<Icon as={Home} />}
+              />
+            </Box>
+            <Box>
+              <FormLabel htmlFor="phoneNumber">ADDRESS</FormLabel>
+              <Field
+                as={CustomInput}
+                name="phoneNumber"
+                id="phoneNumber"
+                type="string"
+                placeholder="123-456-7890"
+                icon={<Icon as={Phone} />}
+              />
+            </Box>
+          </SimpleGrid>
+          <Box paddingTop="10px">
+            <FormLabel htmlFor="address">ADDRESS</FormLabel>
+            <Field
+              as={CustomInput}
+              id="address"
+              name="address"
+              type="string"
+              placeholder="123 Really Good School St"
+              icon={<Icon as={Navigation} />}
+            />
+          </Box>
+          <Box paddingTop="10px">
+            <FormLabel htmlFor="dismissalTime">
+              DISMISSAL TIME (OPTIONAL)
+            </FormLabel>
+            <Field
+              as={CustomInput}
+              id="dismissalTime"
+              name="dismissalTime"
+              type="string"
+              placeholder="Enter dismissal time of school or daycare"
+              icon={<Icon as={Clock} />}
+            />
+          </Box>
+        </FormControl>
+      </Form>
+
+      {/* TODO: trigger on submit when save child information button is clicked */}
+    </Formik>
   );
 };
 

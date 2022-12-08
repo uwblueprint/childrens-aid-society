2bb97d9c52cc6990d74dfff0805884b7bacfb49d
juthikahoque <juthika1@yahoo.com>
2022-12-08 12:16:49 -0500

    add button to child provider form

--- frontend/src/components/intake/child-information/ChildProviderForm.tsx
+++ frontend/src/components/intake/child-information/ChildProviderForm.tsx
@@ -1,13 +1,21 @@
 import { Box, Button, useDisclosure } from "@chakra-ui/react";
 import React from "react";
 import ExistingProvider from "../ExistingProviderModal";
+import NewProviderModal from "../NewProviderModal";
 
 const ChildProviderForm = (): React.ReactElement => {
   const { onOpen, isOpen, onClose } = useDisclosure();
+  const {
+    onOpen: onOpen2,
+    isOpen: isOpen2,
+    onClose: onClose2,
+  } = useDisclosure();
   return (
     <Box>
       <Button onClick={onOpen}>Select providers</Button>
       <ExistingProvider isOpen={isOpen} onClose={onClose} />
+      <Button onClick={onOpen2}>New providers</Button>
+      <NewProviderModal isOpen={isOpen2} onClose={onClose2} />
     </Box>
   );
 };

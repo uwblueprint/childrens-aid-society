8553f360d0bbdb54db20d3eda42ee10beb700501
juthikahoque <juthika1@yahoo.com>
2022-12-08 13:35:09 -0500

    caregiver modal

--- frontend/src/components/pages/IndividualDetails.tsx
+++ frontend/src/components/pages/IndividualDetails.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Box, Button, useDisclosure } from "@chakra-ui/react";
+import NewCaregiverModal from "../intake/NewCaregiverModal";
 
 const IndividualDetails = (): React.ReactElement => {
-  return <Heading>Individual Details Page</Heading>;
+  const { onOpen, isOpen, onClose } = useDisclosure();
+  return (
+    <Box>
+      <Heading>Individual Details Page</Heading>
+      <Button onClick={onOpen}>Add caregivers</Button>
+      <NewCaregiverModal isOpen={isOpen} onClose={onClose} />
+    </Box>
+  );
 };
 
 export default IndividualDetails;

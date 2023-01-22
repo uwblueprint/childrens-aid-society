6b959b1a29412e318019d5c17b6464701a34fed5
Eugene Zhang <zhang.eug@gmail.com>
2023-01-22 18:06:00 -0500

    Implement ‘Providers’ box within Add Child

--- frontend/src/components/intake/PromptBox.tsx
+++ frontend/src/components/intake/PromptBox.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from "react";
-import { Button, VStack, Text } from "@chakra-ui/react";
+import { Button, VStack, Text, HStack } from "@chakra-ui/react";
 
 export type PromptBoxProps = {
   headerText: string;
@@ -44,6 +44,69 @@ const PromptBox = ({
       >
         {buttonText}
       </Button>
+    </VStack>
+  );
+};
+
+export type DoublePromptBoxProps = {
+  headerText: string;
+  descriptionText: string;
+  buttonText1: string;
+  buttonIcon1: ReactElement;
+  onButtonClick1: () => void;
+  buttonText2: string;
+  buttonIcon2: ReactElement;
+  onButtonClick2: () => void;
+};
+
+export const DoublePromptBox = ({
+  headerText,
+  descriptionText,
+  buttonText1,
+  buttonIcon1,
+  onButtonClick1,
+  buttonText2,
+  buttonIcon2,
+  onButtonClick2,
+}: DoublePromptBoxProps): React.ReactElement => {
+  return (
+    <VStack
+      bg="gray.50"
+      borderRadius="14px"
+      borderWidth="1px"
+      borderColor="gray.100"
+      align="flex-end"
+      w="full"
+      maxW={816}
+      padding="32px"
+      spacing="16px"
+    >
+      <VStack align="flex-start" w="full" spacing="8px">
+        <Text color="b&w.black" textStyle="title-large">
+          {headerText}
+        </Text>
+        <Text color="gray.600" textStyle="body-large">
+          {descriptionText}
+        </Text>
+      </VStack>
+      <HStack>
+        <Button
+          variant="tertiary"
+          colorScheme="gray.600"
+          leftIcon={buttonIcon1}
+          onClick={onButtonClick1}
+        >
+          {buttonText1}
+        </Button>
+        <Button
+          variant="secondary"
+          colorScheme="gray.600"
+          leftIcon={buttonIcon2}
+          onClick={onButtonClick2}
+        >
+          {buttonText2}
+        </Button>
+      </HStack>
     </VStack>
   );
 };
--- frontend/src/components/intake/child-information/ChildProviderForm.tsx
+++ frontend/src/components/intake/child-information/ChildProviderForm.tsx
@@ -1,7 +1,9 @@
-import { Box, Button, useDisclosure } from "@chakra-ui/react";
+import { Box, useDisclosure, Icon, VStack } from "@chakra-ui/react";
 import React from "react";
+import { UserPlus } from "react-feather";
 import ExistingProvider from "../ExistingProviderModal";
 import NewProviderModal from "../NewProviderModal";
+import { DoublePromptBox } from "../PromptBox";
 
 const ChildProviderForm = (): React.ReactElement => {
   const {
@@ -15,18 +17,30 @@ const ChildProviderForm = (): React.ReactElement => {
     onClose: onCloseNewProviders,
   } = useDisclosure();
   return (
-    <Box>
-      <Button onClick={onOpenExistingProviders}>Select providers</Button>
-      <ExistingProvider
-        isOpen={isOpenExistingProviders}
-        onClose={onCloseExistingProviders}
-      />
-      <Button onClick={onOpenNewProviders}>New providers</Button>
-      <NewProviderModal
-        isOpen={isOpenNewProviders}
-        onClose={onCloseNewProviders}
-      />
-    </Box>
+    <>
+      <VStack padding="100px">
+        <DoublePromptBox
+          headerText="Providers"
+          descriptionText="At least one provider must be indicated for each child"
+          buttonText1="Add new provider"
+          buttonIcon1={<Icon as={UserPlus} w="16px" h="16px" />}
+          onButtonClick1={onOpenNewProviders}
+          buttonText2="Select providers"
+          buttonIcon2={<Icon as={UserPlus} w="0px" margin="0px" />}
+          onButtonClick2={onOpenExistingProviders}
+        />
+        <Box>
+          <ExistingProvider
+            isOpen={isOpenExistingProviders}
+            onClose={onCloseExistingProviders}
+          />
+          <NewProviderModal
+            isOpen={isOpenNewProviders}
+            onClose={onCloseNewProviders}
+          />
+        </Box>
+      </VStack>
+    </>
   );
 };
 

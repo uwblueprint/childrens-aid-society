87182efad98a182492f15c3707c21de2955e6b0b
deenp03 <100820265+deenp03@users.noreply.github.com>
2023-01-23 20:38:09 -0500

    Implement Program Detail page form fields (#122)
    * Implement Program Detail page form fields
    
    * fixed error
    
    * fixed changes and linted code
    
    * fixed changes
    
    Co-authored-by: jedideen2 <100820265+jedideen2@users.noreply.github.com>

--- frontend/src/components/intake/ProgramForm.tsx
+++ frontend/src/components/intake/ProgramForm.tsx
@@ -2,16 +2,31 @@ import React from "react";
 import {
   Button,
   FormControl,
-  Heading,
   Text,
-  Flex,
   Icon,
+  SimpleGrid,
+  Box,
+  FormLabel,
 } from "@chakra-ui/react";
-import { UserPlus } from "react-feather";
-import { Form, Formik } from "formik";
+import {
+  Truck,
+  Clipboard,
+  Calendar,
+  CheckSquare,
+  TrendingUp,
+  UserPlus,
+} from "react-feather";
+import { Field, Form, Formik } from "formik";
+import CustomInput from "../common/CustomInput";
+import OptionalLabel from "./OptionalLabel";
 
 export type ProgramDetails = {
-  test: string;
+  transportationRequirements: string;
+  schedulingRequirements: string;
+  suggestedStartDate: string;
+  shortTermGoals: string;
+  longTermGoals: string;
+  familialConcerns: string;
 };
 
 type ProgramFormProps = {
@@ -34,18 +49,106 @@ const ProgramForm = ({
   return (
     <Formik initialValues={programDetails} onSubmit={onSubmit}>
       {({ handleSubmit }) => (
-        <Form style={{ padding: "2rem 12rem" }}>
-          <Heading textStyle="heading">Program Details</Heading>
-          <FormControl style={{ padding: "30px" }}>
-            <Flex style={{ padding: "30px" }}>
-              <Text align="left" flex="1" textStyle="title-medium">
-                Other permitted individuals
-              </Text>
-              <Button leftIcon={<Icon as={UserPlus} />} variant="secondary">
-                Add
-              </Button>
-            </Flex>
+        <Form>
+          <Text textAlign="left" textStyle="title-medium">
+            Logistic Needs
+          </Text>
+          <FormControl>
+            <SimpleGrid columns={3} spacing="30px">
+              <Box>
+                <FormLabel htmlFor="transportationRequirements">
+                  TRANSPORTATION REQUIREMENTS
+                </FormLabel>
+                <Field
+                  as={CustomInput}
+                  id="transportationRequirements"
+                  name="transportationRequirements"
+                  type="string"
+                  placeholder="Select an option..."
+                  icon={<Icon as={Truck} />}
+                />
+              </Box>
+              <Box>
+                <FormLabel htmlFor="schedulingRequirements">
+                  SCHEDULING REQUIREMENTS
+                </FormLabel>
+                <Field
+                  as={CustomInput}
+                  // TODO change to list selector component
+                  name="schedulingRequirements"
+                  id="schedulingRequirements"
+                  type="string"
+                  placeholder="Select an option..."
+                  icon={<Icon as={Clipboard} />}
+                />
+              </Box>
+              <Box>
+                <FormLabel htmlFor="suggestedStartDate">
+                  SUGGESTED START DATE
+                </FormLabel>
+                <Field
+                  as={CustomInput}
+                  name="suggestedStartDate"
+                  id="suggestedStartDate"
+                  type="string"
+                  placeholder="DD/MM/YYYY"
+                  icon={<Icon as={Calendar} />}
+                />
+              </Box>
+            </SimpleGrid>
+            <Text textAlign="left" paddingTop="35px" textStyle="title-medium">
+              Program goals
+            </Text>
+            <SimpleGrid columns={2} spacing="30px">
+              <Box>
+                <FormLabel htmlFor="shortTermGoals">SHORT-TERM GOALS</FormLabel>
+                <Field
+                  as={CustomInput}
+                  // TODO change to list selector component
+                  id="shortTermGoals"
+                  name="shortTermGoals"
+                  placeholder="Select goals..."
+                  icon={<Icon as={CheckSquare} />}
+                />
+              </Box>
+              <Box>
+                <FormLabel htmlFor="longTermGoals">LONG-TERM GOALS</FormLabel>
+                <Field
+                  as={CustomInput}
+                  // TODO change to list selector component
+                  id="longTermGoals"
+                  name="longTermGoals"
+                  placeholder="Select goals..."
+                  icon={<Icon as={TrendingUp} />}
+                />
+              </Box>
+            </SimpleGrid>
+            <Box paddingTop="10px">
+              <FormLabel htmlFor="familialConcerns">
+                FAMILIAL CONCERNS <OptionalLabel />
+              </FormLabel>
+              <Field
+                as={CustomInput}
+                // TODO change to multi option list selector component
+                id="familialConcerns"
+                name="familialConcerns"
+                placeholder="Select familial concerns..."
+              />
+            </Box>
           </FormControl>
+          <Box display="flex" justifyContent="space-between" paddingTop="35px">
+            <Text alignSelf="start" textStyle="title-medium">
+              Other permitted individuals
+            </Text>
+            <Button
+              alignSelf="end"
+              leftIcon={<Icon as={UserPlus} />}
+              variant="secondary"
+              mr={2}
+            >
+              Add
+            </Button>
+          </Box>
           <Button
             onClick={() => {
               handleSubmit();

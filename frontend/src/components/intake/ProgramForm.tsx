1d8b5791dd126060865a3a5226fbfe9e8e866aee
thomaswu@uwblueprint.org <thomaswu@uwblueprint.org>
2023-01-26 15:19:30 -0500

    run lint

--- frontend/src/components/intake/ProgramForm.tsx
+++ frontend/src/components/intake/ProgramForm.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import {
-  Button,
   FormControl,
   Text,
   Icon,
@@ -14,12 +13,10 @@ import {
   Calendar,
   CheckSquare,
   TrendingUp,
-  UserPlus,
 } from "react-feather";
-import { Field, Form,  FormikProvider, useFormik } from "formik";
+import { Field, Form, FormikProvider, useFormik } from "formik";
 import CustomInput from "../common/CustomInput";
 import OptionalLabel from "./OptionalLabel";
-
 
 export type ProgramDetails = {
   transportationRequirements: string;
@@ -55,100 +52,100 @@ const ProgramForm = ({
   }, []);
 
   return (
-<FormikProvider value={formik}>
-        <Form>
-          <Text textAlign="left" textStyle="title-medium">
-            Logistic Needs
-          </Text>
-          <FormControl>
-            <SimpleGrid columns={3} spacing="30px">
-              <Box>
-                <FormLabel htmlFor="transportationRequirements">
-                  TRANSPORTATION REQUIREMENTS
-                </FormLabel>
-                <Field
-                  as={CustomInput}
-                  id="transportationRequirements"
-                  name="transportationRequirements"
-                  type="string"
-                  placeholder="Select an option..."
-                  icon={<Icon as={Truck} />}
-                />
-              </Box>
-              <Box>
-                <FormLabel htmlFor="schedulingRequirements">
-                  SCHEDULING REQUIREMENTS
-                </FormLabel>
-                <Field
-                  as={CustomInput}
-                  // TODO change to list selector component
-                  name="schedulingRequirements"
-                  id="schedulingRequirements"
-                  type="string"
-                  placeholder="Select an option..."
-                  icon={<Icon as={Clipboard} />}
-                />
-              </Box>
-              <Box>
-                <FormLabel htmlFor="suggestedStartDate">
-                  SUGGESTED START DATE
-                </FormLabel>
-                <Field
-                  as={CustomInput}
-                  name="suggestedStartDate"
-                  id="suggestedStartDate"
-                  type="string"
-                  placeholder="DD/MM/YYYY"
-                  icon={<Icon as={Calendar} />}
-                />
-              </Box>
-            </SimpleGrid>
-            <Text textAlign="left" paddingTop="35px" textStyle="title-medium">
-              Program goals
-            </Text>
-            <SimpleGrid columns={2} spacing="30px">
-              <Box>
-                <FormLabel htmlFor="shortTermGoals">SHORT-TERM GOALS</FormLabel>
-                <Field
-                  as={CustomInput}
-                  // TODO change to list selector component
-                  id="shortTermGoals"
-                  name="shortTermGoals"
-                  placeholder="Select goals..."
-                  icon={<Icon as={CheckSquare} />}
-                />
-              </Box>
-              <Box>
-                <FormLabel htmlFor="longTermGoals">LONG-TERM GOALS</FormLabel>
-                <Field
-                  as={CustomInput}
-                  // TODO change to list selector component
-                  id="longTermGoals"
-                  name="longTermGoals"
-                  placeholder="Select goals..."
-                  icon={<Icon as={TrendingUp} />}
-                />
-              </Box>
-            </SimpleGrid>
-            <Box paddingTop="10px">
-              <FormLabel htmlFor="familialConcerns">
-                FAMILIAL CONCERNS <OptionalLabel />
+    <FormikProvider value={formik}>
+      <Form>
+        <Text textAlign="left" textStyle="title-medium">
+          Logistic Needs
+        </Text>
+        <FormControl>
+          <SimpleGrid columns={3} spacing="30px">
+            <Box>
+              <FormLabel htmlFor="transportationRequirements">
+                TRANSPORTATION REQUIREMENTS
               </FormLabel>
               <Field
                 as={CustomInput}
-                // TODO change to multi option list selector component
-                id="familialConcerns"
-                name="familialConcerns"
-                placeholder="Select familial concerns..."
+                id="transportationRequirements"
+                name="transportationRequirements"
+                type="string"
+                placeholder="Select an option..."
+                icon={<Icon as={Truck} />}
               />
             </Box>
-          </FormControl>
-          <Box display="flex" justifyContent="space-between" paddingTop="35px">
-            <Text alignSelf="start" textStyle="title-medium">
-              Other permitted individuals
-            </Text>
+            <Box>
+              <FormLabel htmlFor="schedulingRequirements">
+                SCHEDULING REQUIREMENTS
+              </FormLabel>
+              <Field
+                as={CustomInput}
+                // TODO change to list selector component
+                name="schedulingRequirements"
+                id="schedulingRequirements"
+                type="string"
+                placeholder="Select an option..."
+                icon={<Icon as={Clipboard} />}
+              />
+            </Box>
+            <Box>
+              <FormLabel htmlFor="suggestedStartDate">
+                SUGGESTED START DATE
+              </FormLabel>
+              <Field
+                as={CustomInput}
+                name="suggestedStartDate"
+                id="suggestedStartDate"
+                type="string"
+                placeholder="DD/MM/YYYY"
+                icon={<Icon as={Calendar} />}
+              />
+            </Box>
+          </SimpleGrid>
+          <Text textAlign="left" paddingTop="35px" textStyle="title-medium">
+            Program goals
+          </Text>
+          <SimpleGrid columns={2} spacing="30px">
+            <Box>
+              <FormLabel htmlFor="shortTermGoals">SHORT-TERM GOALS</FormLabel>
+              <Field
+                as={CustomInput}
+                // TODO change to list selector component
+                id="shortTermGoals"
+                name="shortTermGoals"
+                placeholder="Select goals..."
+                icon={<Icon as={CheckSquare} />}
+              />
+            </Box>
+            <Box>
+              <FormLabel htmlFor="longTermGoals">LONG-TERM GOALS</FormLabel>
+              <Field
+                as={CustomInput}
+                // TODO change to list selector component
+                id="longTermGoals"
+                name="longTermGoals"
+                placeholder="Select goals..."
+                icon={<Icon as={TrendingUp} />}
+              />
+            </Box>
+          </SimpleGrid>
+          <Box paddingTop="10px">
+            <FormLabel htmlFor="familialConcerns">
+              FAMILIAL CONCERNS <OptionalLabel />
+            </FormLabel>
+            <Field
+              as={CustomInput}
+              // TODO change to multi option list selector component
+              id="familialConcerns"
+              name="familialConcerns"
+              placeholder="Select familial concerns..."
+            />
           </Box>
-        </Form>
+        </FormControl>
+        <Box display="flex" justifyContent="space-between" paddingTop="35px">
+          <Text alignSelf="start" textStyle="title-medium">
+            Other permitted individuals
+          </Text>
+        </Box>
+      </Form>
     </FormikProvider>
   );
 };

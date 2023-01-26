c8e3246daa28fc25748211604f43f3a601e122e6
thomaswu@uwblueprint.org <thomaswu@uwblueprint.org>
2023-01-25 19:36:32 -0500

    fix display issues with footer + add generic footer feature

--- frontend/src/components/intake/IntakeFormFooter.tsx
+++ frontend/src/components/intake/IntakeFormFooter.tsx
@@ -1,27 +1,108 @@
 import React from "react";
-import { Box, Button } from "@chakra-ui/react";
+import { Button, Flex, useToast } from "@chakra-ui/react";
 
-export type IntakeFooterProps = {
-  nextStep: () => void;
+export type CurStepLayout = {
+  nextBtnTxt: string;
+  backBtnTxt: string;
+  showClearPageBtn: boolean;
 };
 
-const IntakeFooter = ({ nextStep }: IntakeFooterProps): React.ReactElement => {
+export type IntakeFooterProps = {
+  nextBtnRef?: React.RefObject<HTMLButtonElement>;
+  currentStep: number;
+  curStepLayout: Map<number, CurStepLayout>;
+  isStepComplete: () => boolean;
+  registrationLoading: boolean;
+  nextStepCallBack: () => void;
+  prevStepCallBack: () => void;
+};
+
+const IntakeFooter = ({
+  nextBtnRef,
+  currentStep,
+  curStepLayout,
+  isStepComplete,
+  registrationLoading,
+  nextStepCallBack,
+  prevStepCallBack,
+}: IntakeFooterProps): React.ReactElement => {
+  const toast = useToast();
+  const onNextStep = () => {
+    if (isStepComplete()) {
+      nextStepCallBack();
+    } else {
+      toast({
+        title: "Form does not pass validation.",
+        description:
+          "Please complete all form fields according to requirements.",
+        variant: "subtle",
+        duration: 3000,
+        status: "error",
+        position: "top",
+      });
+    }
+  };
+  const onPrevStep = () => {
+    prevStepCallBack();
+  };
+
   return (
-    <Box bg="gray.50" h="90px">
+    <Flex
+      bg="gray.50"
+      boxShadow="sm"
+      minH="92px"
+      width="100vw"
+      align="center"
+      justify={{ sm: "center", md: "end" }}
+      flexWrap="wrap"
+      padding="20px"
+      position="fixed"
+      bottom="0"
+      zIndex="5"
+    >
+      {curStepLayout.get(currentStep)?.showClearPageBtn ? (
+        <Button
+          width={{ sm: "95vw", md: "45vw", lg: "auto" }}
+          variant="tertiary"
+          height="48px"
+          mb={{ sm: 4, md: 0 }}
+          mr={{ sm: 0, md: 4 }}
+        >
+          Clear page
+        </Button>
+      ) : (
+        ""
+      )}
       <Button
+        ref={nextBtnRef}
+        width={{ sm: "95vw", md: "45vw", lg: "auto" }}
+        height="48px"
+        loadingText="Submitting"
         type="submit"
-        ml="1200px"
-        mt="25px"
+        isLoading={registrationLoading}
+        mb={{ sm: 4, md: 0 }}
+        mr={{ sm: 0, md: 4 }}
         onClick={() => {
-          nextStep();
+          onPrevStep();
         }}
       >
-        Next Section
+        {curStepLayout.get(currentStep)?.backBtnTxt}
       </Button>
-      <Button variant="tertiary" ml="1050px" mt="-65px">
-        Clear page
+
+      <Button
+        ref={nextBtnRef}
+        width={{ sm: "95vw", md: "45vw", lg: "auto" }}
+        height="48px"
+        loadingText="Submitting"
+        type="submit"
+        isLoading={registrationLoading}
+        onClick={() => {
+          onNextStep();
+        }}
+      >
+        {curStepLayout.get(currentStep)?.nextBtnTxt}
       </Button>
-    </Box>
+    </Flex>
   );
 };
 

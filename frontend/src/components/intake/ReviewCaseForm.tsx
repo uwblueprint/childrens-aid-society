5e15671c781f718e63509979d88f09649def2dff
Eugene Zhang <zhang.eug@gmail.com>
2023-01-29 18:35:02 -0500

    Create initial individual details template

--- frontend/src/components/intake/ReviewCaseForm.tsx
+++ frontend/src/components/intake/ReviewCaseForm.tsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, VStack, Text, HStack, Icon } from "@chakra-ui/react";
+import { Edit2 } from "react-feather";
+import { IndividualDetailsOverview } from "./PromptBox";
 
 type ReviewFormProps = {
   prevStep: () => void;
+  title: string;
+  childrenDetails?: IndividualDetailsOverview[];
+  caregiverDetails?: IndividualDetailsOverview[];
 };
 
-const ReviewForm = ({ prevStep }: ReviewFormProps): React.ReactElement => {
+const ReviewForm = ({
+  prevStep,
+  title,
+  childrenDetails,
+  caregiverDetails,
+}: ReviewFormProps): React.ReactElement => {
   return (
     <>
+      <VStack padding="32px">
+        <HStack w="full" display="flex" justifyContent="space-between">
+          <Text color="b&w.black" textStyle="header-large">
+            {title}
+          </Text>
+          <Button
+            textStyle="button-medium"
+            variant="primary"
+            rightIcon={<Icon as={Edit2} h="16px" />}
+          >
+            Edit
+          </Button>
+        </HStack>
+      </VStack>
+
       <Box>
         <Button
           onClick={() => {
--- frontend/src/components/pages/IntakePage.tsx
+++ frontend/src/components/pages/IntakePage.tsx
@@ -80,7 +80,7 @@ const Intake = (): React.ReactElement => {
         return (
           <>
             <Box style={{ textAlign: "center", padding: "30px 0px 40px 0px" }}>
-              <ReviewForm prevStep={prevStep} />
+              <ReviewForm prevStep={prevStep} title="Individual details" />
             </Box>
           </>
         );

48d79b87ecccf15ec93f22fe8500087b3a5ce2d6
Ivan Yu <85257356+IvanYu327@users.noreply.github.com>
2023-02-02 00:19:40 -0500

    Add footer with save button to child form (#124)
    * Add onchange listeneres and child info form footer
    
    * Add comment
    
    * lint
    
    * lint
    
    * make footer stick to bottom of window
    
    * Fix footer overlapping the intake form
    
    ---------
    
    Co-authored-by: Bowen Zhu <43287170+bowenzhu1@users.noreply.github.com>

--- frontend/src/components/intake/child-information/AddChildPage.tsx
+++ frontend/src/components/intake/child-information/AddChildPage.tsx
@@ -1,4 +1,4 @@
-import { Button, Icon, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Icon, Text, VStack } from "@chakra-ui/react";
 import React, { useState } from "react";
 import { ChevronLeft } from "react-feather";
 import { useHistory } from "react-router-dom";
@@ -11,6 +11,7 @@ import SchoolDaycareForm, { SchoolDetails } from "./SchoolDaycareForm";
 const AddChild = (): React.ReactElement => {
   const [activeFormIndex, setActiveFormIndex] = useState(0);
   const history = useHistory();
+
   const [childDetails, setChildDetails] = useState<ChildDetails>({
     childName: "",
     cpinFileNumber: "",
@@ -25,6 +26,17 @@ const AddChild = (): React.ReactElement => {
     schoolAddress: "",
     dismissalTime: "",
   });
+
+  const requiredInfomationMissing: boolean =
+    !childDetails.childName ||
+    !childDetails.cpinFileNumber ||
+    !childDetails.dateOfBirth;
+
+  // TODO: Check other required fields
+
+  const childFormSubmitHandler = () => {
+    // TODO: Do something with the information
+  };
 
   const renderChildForm = () => {
     switch (activeFormIndex) {
@@ -79,8 +91,30 @@ const AddChild = (): React.ReactElement => {
           onClick={setActiveFormIndex}
         />
       </VStack>
-      {renderChildForm()}
-      {/* TODO: Add footer component */}
+
+      <Box marginBottom="100px">{renderChildForm()}</Box>
+
+      <Box
+        bg="white"
+        height="87px"
+        bottom="0"
+        width="100%"
+        display="flex"
+        justifyContent="flex-end"
+        alignItems="center"
+        position="fixed"
+        shadow="0px -4px 12px rgba(226, 225, 236, 0.4), 0px -8px 24px rgba(226, 225, 236, 0.25)"
+      >
+        <Button
+          type="submit"
+          mr="96px"
+          disabled={requiredInfomationMissing}
+          onClick={childFormSubmitHandler}
+        >
+          Save child information
+        </Button>
+        {/* TODO: Add cancel button */}
+      </Box>
     </>
   );
 };

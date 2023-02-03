f6a0a1a5a1c25f8dd679272894a1c9c400cfbb9e
Eugene Zhang <86681988+Ezzhingy@users.noreply.github.com>
2023-02-03 01:12:58 -0500

    Review Case - Case Referral (#133)
    * Implement ‘Providers’ box within Add Child
    
    * Combine PromptBox components
    
    * Conditionally render second button in PromptBox
    
    * Conditionally update PromptBox container with Individual Detail
    
    * Add delete functionality to PromptBox
    
    * Remove test objects
    
    * Edit styling and fix rendering functionality
    
    * Make small font changes
    
    * Create initial individual details template
    
    * Implement individual details in Review Case
    
    * Delete test cases
    
    * Create IndividualDetails component
    
    * Implement Case Referral
    
    * Make readOnly prop optional

--- frontend/src/components/intake/ReviewCaseForm.tsx
+++ frontend/src/components/intake/ReviewCaseForm.tsx
@@ -1,14 +1,45 @@
 import React from "react";
-import { Box, Button } from "@chakra-ui/react";
+import { Box, Button, HStack, Text, Icon, Stack } from "@chakra-ui/react";
+import { Edit2 } from "react-feather";
 import IndividualDetails from "./IndividualDetails";
+import ReferralForm, { ReferralDetails } from "./ReferralForm";
 
 type ReviewFormProps = {
+  referralDetails: ReferralDetails;
+  setReferralDetails: React.Dispatch<React.SetStateAction<ReferralDetails>>;
+  nextStep: () => void;
   prevStep: () => void;
 };
 
-const ReviewForm = ({ prevStep }: ReviewFormProps): React.ReactElement => {
+const ReviewForm = ({
+  referralDetails,
+  setReferralDetails,
+  nextStep,
+  prevStep,
+}: ReviewFormProps): React.ReactElement => {
   return (
     <>
+      <Stack padding="32px" spacing="16px">
+        <HStack w="full" display="flex" justifyContent="space-between">
+          <Text color="b&w.black" textStyle="header-large">
+            Case referral
+          </Text>
+          <Button
+            textStyle="button-medium"
+            variant="primary"
+            rightIcon={<Icon as={Edit2} h="16px" />}
+          >
+            Edit {/* TODO: implement edit button */}
+          </Button>
+        </HStack>
+        <ReferralForm
+          referralDetails={referralDetails}
+          setReferralDetails={setReferralDetails}
+          nextStep={nextStep}
+          readOnly
+        />
+      </Stack>
+
       <IndividualDetails
         title="Individual details"
         childrenDetails={[]}

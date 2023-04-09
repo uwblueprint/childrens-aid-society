ceb0fbf74159f68098c8b6516e78dd71e30a487a
thomaswu@uwblueprint.org <thomaswu@uwblueprint.org>
2023-04-09 17:16:27 -0400

    run lint

--- backend/python/tools/db_wipe.py
+++ backend/python/tools/db_wipe.py
@@ -1,14 +1,16 @@
 from app import create_app
 from app.models import db
 
-
 """
 Wipe postgres db as well as any associated auth entried within firebase auth store
 """
 
 app = create_app("development")
 
-from app.rest.user_routes import user_service # we have to import this after create_app since application context hasn't been pushed onto stack yet
+from app.rest.user_routes import (  # we have to import this after create_app since application context hasn't been pushed onto stack yet
+    user_service,
+)
+
 for userDTO in user_service.get_users():
     user_service.delete_user_by_id(userDTO.id)
 

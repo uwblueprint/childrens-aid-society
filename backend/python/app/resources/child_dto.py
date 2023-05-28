3e481f54021c3bea931b209464810649c7faccc6
Bowen Zhu <bowenzhu@Bowens-MacBook-Air.local>
2023-05-28 11:44:53 -0400

    fix child date_of_birth validate

--- backend/python/app/resources/child_dto.py
+++ backend/python/app/resources/child_dto.py
@@ -35,12 +35,7 @@ class CreateChildDTO(ChildDTO):
         if self.intake_id and not type(self.intake_id) == int:
             error_list.append("The intake_id supplied is invalid")
         if self.date_of_birth:
-            if type(self.date_of_birth) == str:
-                try:
-                    datetime.datetime.strptime(self.date_of_birth, "%Y-%m-%d")
-                except ValueError:
-                    error_list.append("The date_of_birth supplied is invalid")
-            elif not type(self.date_of_birth) == datetime.date:
+            if not type(self.date_of_birth) == str:
                 error_list.append("The date_of_birth supplied is invalid")
         if self.cpin_number and not type(self.cpin_number) == str:
             error_list.append("The cpin_number supplied is invalid")

14d380553f13723a278672f5bc477df34e9d988a
Bowen Zhu <bowenzhu@Bowens-MacBook-Air.local>
2023-06-01 20:32:05 -0400

    test data and scripts

---
+++ backend/python/app/rest/testbad.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d @testbad.json http://localhost:5000/intake > testbad.html
---
+++ backend/python/app/rest/testgood.sh
@@ -0,0 +1,1 @@
+curl -X POST -H "Content-Type: application/json" -d @testgood.json http://localhost:5000/intake > testgood.html

8b28bedfe78dd3714b898329453a4826c7ac1468
sohailsayed <s7sayed@uwaterloo.ca>
2023-06-25 01:52:12 -0400

    add components to Cases page

--- frontend/src/components/pages/CasesPage.tsx
+++ frontend/src/components/pages/CasesPage.tsx
@@ -1,5 +1,6 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Button, Flex, Icon, Text } from "@chakra-ui/react";
 import React from "react";
+import { ArrowLeft } from "react-feather";
 import IntakeHeader from "../intake/IntakeHeader";
 import CaseStatus from "../../types/CaseStatus";
 import CaseCard, { CaseCardProps } from "../dashboard/CaseCard";
@@ -72,24 +73,45 @@ const Cases = (): React.ReactElement => {
         secondaryTitle="Case Management"
         hasLogout
       />
-      <Box>
+      <Box px="80px" pt="32px">
         {/* Return Button */}
-        {/* Appropriate Header */}
-        {/* Grid of Appropriate Filtered Case components */}
-        {cases.active.map((caseData: CaseCardProps) => {
-          return (
-            <CaseCard
-              key={caseData.caseTitle}
-              caseTitle={caseData.caseTitle}
-              caseLead={caseData.caseLead}
-              date={caseData.date}
-              familyName={caseData.familyName}
-              caseTag={caseData.caseTag}
-            />
-          );
-        })}
-        ;{/* Load more cases button (doesn't need to work) */}
+        <Icon as={ArrowLeft} boxSize={8} />
       </Box>
+      {/* Appropriate Header */}
+      <Text textStyle="header-large" px="86px" pt="32px">
+        Active Cases
+      </Text>
+      <Box px="86px" pt="32px">
+        {/* Grid of Appropriate Filtered Case components */}
+        <Flex justifyContent="space-between">
+          {cases.active.map((caseData: CaseCardProps) => {
+            return (
+              <CaseCard
+                key={caseData.caseTitle}
+                caseTitle={caseData.caseTitle}
+                caseLead={caseData.caseLead}
+                date={caseData.date}
+                familyName={caseData.familyName}
+                caseTag={caseData.caseTag}
+              />
+            );
+          })}
+        </Flex>
+      </Box>
+      {/* Load more cases button (doesn't need to work) */}
+      <Flex justifyContent="center" pt="45px">
+        <Button
+          variant="ghost"
+          px="2"
+          rounded="lg"
+          border="1px"
+          borderColor="blue.400"
+          color="blue.400"
+          bg="blue.50"
+        >
+          Load More Cases
+        </Button>
+      </Flex>
     </Box>
   );
 };

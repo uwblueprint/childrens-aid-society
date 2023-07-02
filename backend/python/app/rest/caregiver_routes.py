23e6994e4183866521555f5112384f13abc73ef0
Jenny Vong <jennyvong@Jennys-MacBook-Pro-2.local>
2023-07-02 11:13:33 -0400

    Implement Update Caregiver API

--- backend/python/app/rest/caregiver_routes.py
+++ backend/python/app/rest/caregiver_routes.py
@@ -20,6 +20,19 @@ def get_all_caregivers():
     except Exception as error:
         return jsonify(error), 400
 
+# get all caregivers by intake_id
+@blueprint.route("/<int:intake_id>", methods=["GET"], strict_slashes=False)
+# @require_authorization_by_role({"Admin"})
+def get_caregivers_by_intake_id(intake_id):
+    if intake_id:
+        if type(intake_id) is not int:
+            return jsonify({"error:" "intake_id query parameter must be an int"})
+    try:
+        caregivers = caregiver_service.get_all_caregivers()
+        return jsonify(list(map(lambda user: user.__dict__, caregivers))), 200
+    except Exception as error:
+        return jsonify(error), 400
+
 
 # create a caregiver
 @blueprint.route("/", methods=["POST"], strict_slashes=False)
@@ -30,5 +43,19 @@ def create_caregiver():
         caregiver = CreateCaregiverDTO(**request.get_json())
         new_caregiver = caregiver_service.create_caregiver(caregiver)
         return jsonify(new_caregiver.__dict__), 201
+    except Exception as error:
+        return jsonify(str(error)), 400
+
+
+# update a caregiver
+@blueprint.route("/<int:caregiver_id>", methods=["PUT"], strict_slashes=False)
+def update_caregiver(caregiver_id):
+    try:
+        new_caregiver = request.json
+        updated_caregiver = caregiver_service.update_caregiver(
+            caregiver_id, new_caregiver
+        )
+        return jsonify(updated_caregiver.__dict__), 200
+
     except Exception as error:
         return jsonify(str(error)), 400

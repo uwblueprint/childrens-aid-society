85afd641b4091e316bc812ad00db716352636037
kevinasian122 <yanchenkai@gmail.com>
2023-08-17 21:39:20 -0400

    Implement visit component skeleton

--- frontend/src/components/pages/VisitPage.tsx
+++ frontend/src/components/pages/VisitPage.tsx
@@ -1,11 +1,75 @@
 import React from "react";
-import { Text } from "@chakra-ui/react";
+import { Box, Button, Flex, Heading, Text } from "@chakra-ui/react";
+import { ArrowLeft } from "react-feather";
+import { useParams } from "react-router-dom";
 
 const Visit = (): React.ReactElement => {
+  // url is /visit/caseId/visitId
+  const params = useParams();
+  console.log(params);
   return (
-    <div style={{ textAlign: "center", paddingTop: "20px" }}>
-      <Text textStyle="header-large">Visit 📝</Text>
-    </div>
+    <>
+      <Box bg="gray.50" style={{ padding: "50px 100px 25px 100px" }}>
+        <Text color="gray.600" textStyle="title-small">
+          Case management
+        </Text>
+        <Flex>
+          <Heading textStyle="display-medium">New Visit Log</Heading>
+        </Flex>
+      </Box>
+      <Box
+        width="100%"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box display="flex" pt="10" gap="32">
+          <Box
+            width="40%"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "2vh",
+              fontWeight: "bold",
+            }}
+          >
+            <Box
+              padding="0 0 30px 0"
+              fontWeight="bold"
+              display="flex"
+              gap="8px"
+            >
+              <ArrowLeft /> Save and Exit {/* TODO implement save and exit */}
+            </Box>
+            <Button variant="primary" width="fit-content">
+              Child Information
+            </Button>
+            <Box>Visit Timestamp</Box>
+            <Box>Attendance</Box>
+            <Box>Transportation</Box>
+            <Box>Child and Family Support Worker</Box>
+          </Box>
+          <Box
+            width="80%"
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              gap: "3vh",
+            }}
+          >
+            <Text textStyle="header-medium">Child Information</Text>
+            <Text textStyle="header-medium">Visit Timestamp</Text>
+            <Text textStyle="header-medium">Attendance</Text>
+            <Text textStyle="header-medium">Transportation</Text>
+            <Text textStyle="header-medium">Notes</Text>
+            <Text textStyle="header-medium">
+              Child and Family Support Worker
+            </Text>
+          </Box>
+        </Box>
+      </Box>
+    </>
   );
 };
 

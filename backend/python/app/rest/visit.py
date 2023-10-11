d92fad36c5c3a072418383fd0b7f380a9c4ae165
joshuaye <j96ye@uwaterloo.ca>
2023-10-11 11:04:37 -0400

    Run linter and instantiate visit.py

--- backend/python/app/rest/visit.py
+++ backend/python/app/rest/visit.py
@@ -2,10 +2,14 @@ from flask import Blueprint, current_app, jsonify, request
 
 from ..middlewares.auth import require_authorization_by_role
 from ..middlewares.validate import validate_request
-from ..resources.attendance_records_dto import CreateAttendanceRecordsDTO
-from ..services.implementations.attendance_record_service import AttendanceRecordService
+from ..services.implementations.attendance_sheet_service import AttendanceSheetService
 
-attendance_record_service = AttendanceRecordService(current_app.logger)
+attendance_sheet_service = AttendanceSheetService(current_app.logger)
+
+from ..resources.attendance_records_dto import CreateAttendanceRecordsDTO
+
+# from ..services.implementations.attendance_record_service import AttendanceRecordService
+# attendance_record_service = AttendanceRecordService(current_app.logger)
 
 blueprint = Blueprint("attendanceRecord", __name__, url_prefix="/attendanceRecord")
 
@@ -13,23 +17,23 @@ blueprint = Blueprint("attendanceRecord", __name__, url_prefix="/attendanceRecor
 # get all visits
 @blueprint.route("/", methods=["GET"], strict_slashes=False)
 # @require_authorization_by_role({"Admin"})
-def get_all_visit():
-    intake_id = int(request.args.get("intakeId"))
-    id = int(request.args.get("id"))
+def get_visits():
+    intake_id = request.args.get("intakeId")
+    id = request.args.get("id")
     try:
         if intake_id and id:
             return jsonify({"error": "Cannot query by both intake id and id"}), 400
         elif intake_id:
             visits = attendance_sheet_service.get_attendance_sheet_by_intake(
-                intake_id
+                int(intake_id)
             )
             # get attendance_record and combine
         elif id:
-            visits = attendance_sheet_service.get_attendance_sheet_by_id(id)
+            visits = attendance_sheet_service.get_attendance_sheet_by_id(int(id))
             # get attendance_record and combine
         else:
             visits = attendance_sheet_service.get_all_attendance_sheets()
             # get attendance_record and combine
-        return jsonify(list(map(lambda user: user.__dict__, visits))), 200
+        return jsonify(list(map(lambda visit: visit.__dict__, visits))), 200
     except Exception as error:
         return jsonify(error), 400

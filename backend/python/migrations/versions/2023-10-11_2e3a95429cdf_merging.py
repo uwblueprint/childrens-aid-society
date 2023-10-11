e1d2969096b9ea3ee6023ce99af5aaefa6335b47
Eugene Zhang <zhang.eug@gmail.com>
2023-10-11 15:50:57 -0400

    Merge db heads

---
+++ backend/python/migrations/versions/2023-10-11_2e3a95429cdf_merging.py
@@ -0,0 +1,24 @@
+"""merging
+
+Revision ID: 2e3a95429cdf
+Revises: 9102ff64a1fd, bef4bdd6cc2b
+Create Date: 2023-10-11 18:31:26.863225
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "2e3a95429cdf"
+down_revision = ("9102ff64a1fd", "bef4bdd6cc2b")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass

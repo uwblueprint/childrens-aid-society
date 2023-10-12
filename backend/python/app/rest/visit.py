fef896a562d648f4482ec9d390e8fe3671e704d3
Oditha <oditha14@gmail.com>
2023-10-12 00:21:28 -0400

    visit.py for DELETE CRUD function

--- backend/python/app/rest/visit.py
+++ backend/python/app/rest/visit.py
@@ -166,3 +166,19 @@ def update_attendance_record():
 
     except Exception as error:
         return jsonify(str(error)), 400
+
+@blueprint.route("/", methods=["DELETE"], strict_slashes=False)
+def delete_visit():
+    args = request.args
+    id = int(args.get("id"))
+    # Delete attendance by id, requires id
+    if id:
+        try:
+            attendace_sheet_service.delete_attendance_sheet(id)
+            return jsonify({"message": "Attendance sheet deleted successfully"})
+        except Exception as error:
+            return jsonify({"   error": str(error)}), 500
+    else:
+        return jsonify({"error": "Must supply id as a query parameter."}), 400
+
+

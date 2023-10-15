0753b2ddd944f3ac7e5695243ce1efba9513b95e
Kevin Yan <70108329+kevinasian122@users.noreply.github.com>
2023-10-15 14:13:39 -0400

    Kevin y/implement attendance record service (#210)
    * Created attendance sheet service, routes (in visit.py), and test
    
    * Fixed test b/c attendance sheet got updated

--- backend/python/app/rest/visit.py
+++ backend/python/app/rest/visit.py
@@ -2,7 +2,14 @@ from flask import Blueprint, current_app, jsonify, request
 
 from ..resources.attendance_sheet_dto import CreateAttendanceSheetDTO
 from ..services.implementations.attendance_sheet_service import AttendanceSheetService
+from flask import Blueprint, current_app, jsonify, request
 
+from ..middlewares.auth import require_authorization_by_role
+from ..middlewares.validate import validate_request
+from ..resources.attendance_records_dto import CreateAttendanceRecordsDTO
+from ..services.implementations.attendance_record_service import AttendanceRecordService
+
+attendance_record_service = AttendanceRecordService(current_app.logger)
 attendance_sheet_service = AttendanceSheetService(current_app.logger)
 blueprint = Blueprint("visit", __name__, url_prefix="/visit")
 
@@ -21,3 +28,69 @@ def delete_visit():
     else:
         return jsonify({"error": "Must supply id as a query parameter."}), 400
 
+# get all attendance records
+@blueprint.route("/", methods=["GET"], strict_slashes=False)
+# @require_authorization_by_role({"Admin"})
+def get_all_attendance_records():
+    try:
+        records = attendance_record_service.get_all_attendance_records()
+        return jsonify(list(map(lambda record: record.__dict__, records))), 200
+    except Exception as error:
+        return jsonify(error), 400
+
+
+# create a attendance record
+@blueprint.route("/", methods=["POST"], strict_slashes=False)
+# @require_authorization_by_role({"Admin"})
+@validate_request("CreateAttendanceRecordsDTO")
+def create_attendance_record():
+    try:
+        record = CreateAttendanceRecordsDTO(**request.get_json())
+        new_record = attendance_record_service.create_attendance_record(record)
+        return jsonify(new_record.__dict__), 201
+    except Exception as error:
+        return jsonify(str(error)), 400
+
+
+@blueprint.route("/", methods=["DELETE"], strict_slashes=False)
+def delete_attendance_record():
+    """
+    Delete attendance record by record_id specified through a query parameter
+    """
+    record_id = int(request.args.get("record_id"))
+
+    if record_id:
+        if type(record_id) is not int:
+            return (
+                jsonify({"error": "record_id query parameter must be an int"}),
+                400,
+            )
+        else:
+            try:
+                attendance_record_service.delete_attendance_record(record_id)
+                return "attendance record deleted", 200
+            except Exception as e:
+                error_message = getattr(e, "message", None)
+                return (
+                    jsonify({"error": (error_message if error_message else str(e))}),
+                    500,
+                )
+
+    return (
+        jsonify({"error": "Must supply record_id as query parameter."}),
+        400,
+    )
+
+
+@blueprint.route("/", methods=["PUT"], strict_slashes=False)
+def update_attendance_record():
+    record_id = int(request.args.get("record_id"))
+    try:
+        updated_data = request.json
+        updated_record = attendance_record_service.update_attendance_record(
+            record_id, updated_data
+        )
+        return jsonify(updated_record.__dict__), 200
+
+    except Exception as error:
+        return jsonify(str(error)), 400

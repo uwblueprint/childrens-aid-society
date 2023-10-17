27915d46d88f5db58efcd158c1ddeaef4d188b30
Oditha <oditha14@gmail.com>
2023-10-16 21:05:14 -0400

    delete_visit bug fixed

--- backend/python/app/rest/visit.py
+++ backend/python/app/rest/visit.py
@@ -7,6 +7,7 @@ from flask import Blueprint, current_app, jsonify, request
 from ..middlewares.auth import require_authorization_by_role
 from ..middlewares.validate import validate_request
 from ..services.implementations.attendance_sheet_service import AttendanceSheetService
+from ..resources.attendance_sheet_dto import CreateAttendanceSheetDTO
 
 attendance_sheet_service = AttendanceSheetService(current_app.logger)
 
@@ -153,14 +154,13 @@ def update_attendance_record():
 def delete_visit():
     args = request.args
     id = int(args.get("id"))
-    # Delete attendance by id, requires id
     if id:
         try:
             attendace_sheet_service.delete_attendance_sheet(id)
-            return jsonify({"message": "Attendance sheet deleted successfully"})
+
+            return f"Attendance sheet with id: {id} has been deleted", 200
         except Exception as error:
-            return jsonify({"   error": str(error)}), 500
+            return jsonify({"error": str(error)}), 500
     else:
         return jsonify({"error": "Must supply id as a query parameter."}), 400
-
-
+    

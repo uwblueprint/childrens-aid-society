6f48b207bd29a6bc6e10eaa09721a2a4e6d04397
joshuaye <j96ye@uwaterloo.ca>
2023-10-23 23:25:09 -0400

    Implement GET visits endpoints

--- backend/python/app/rest/visit.py
+++ backend/python/app/rest/visit.py
@@ -2,6 +2,7 @@ from flask import Blueprint, current_app, jsonify, request
 
 from ..middlewares.auth import require_authorization_by_role
 from ..middlewares.validate import validate_request
+from ..resources.visit_dto import VisitDTO
 from ..services.implementations.attendance_sheet_service import AttendanceSheetService
 
 attendance_sheet_service = AttendanceSheetService(current_app.logger)
@@ -24,27 +25,48 @@ def get_visits():
         if intake_id and id:
             return jsonify({"error": "Cannot query by both intake id and id"}), 400
         elif intake_id:
-            visits = attendance_sheet_service.get_attendance_sheet_by_intake(
+            records = attendance_record_service.get_attendance_record_by_intake(
                 int(intake_id)
             )
-            # get attendance_record and combine
         elif id:
-            visits = attendance_sheet_service.get_attendance_sheet_by_id(int(id))
-            # get attendance_record and combine
+            records = attendance_record_service.get_attendance_record_by_id(int(id))
         else:
-            visits = attendance_sheet_service.get_all_attendance_sheets()
-            # get attendance_record and combine
+            records = attendance_record_service.get_all_attendance_records()
+
+        visits = []
+        for record in records:
+            sheet = attendance_sheet_service.get_attendance_sheet_by_id(
+                record.attendance_sheet_id
+            )[0]
+
+            childInformation_obj = {
+                "familyName": sheet.family_name,
+                "children": sheet.children,
+                "childServiceWorker": sheet.csw,
+                "childProtectionWorker": sheet.cpw,
+                "fosterCareCoordinator": sheet.fcc,
+            }
+
+            visit = {
+                "user_id": record.id,
+                "childInformation": childInformation_obj,
+                "visitTimestamp": {
+                    "visitDate": record.date,
+                    "visitDay": "",
+                    "visitSupervision": record.supervision,
+                    "startTime": record.start_time,
+                    "endTime": record.end_time,
+                    "location": record.location,
+                },
+                "attendance": {"visitingMembers": []},
+                "transportation": [],
+                "notes": record.comments,
+                "childAndFamilySupportWorker": record.child_family_support_worker_id,
+            }
+            visits.append(visit)
+
+        visits = [VisitDTO(**visit) for visit in visits]
         return jsonify(list(map(lambda visit: visit.__dict__, visits))), 200
-    except Exception as error:
-        return jsonify(error), 400
-      
-# get all attendance records
-@blueprint.route("/", methods=["GET"], strict_slashes=False)
-# @require_authorization_by_role({"Admin"})
-def get_all_attendance_records():
-    try:
-        records = attendance_record_service.get_all_attendance_records()
-        return jsonify(list(map(lambda record: record.__dict__, records))), 200
     except Exception as error:
         return jsonify(error), 400
 

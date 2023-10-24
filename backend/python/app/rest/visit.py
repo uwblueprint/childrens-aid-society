c1a81bce904976c2b3ae1665e4b54d46584c9c01
Oditha <oditha14@gmail.com>
2023-10-23 20:14:27 -0400

    spelling error fix (attendace to attendance)

--- backend/python/app/rest/visit.py
+++ backend/python/app/rest/visit.py
@@ -7,19 +7,29 @@ from flask import Blueprint, current_app, jsonify, request
 from ..middlewares.auth import require_authorization_by_role
 from ..middlewares.validate import validate_request
 from ..services.implementations.attendance_sheet_service import AttendanceSheetService
-from ..resources.attendance_sheet_dto import CreateAttendanceSheetDTO
-
-attendance_sheet_service = AttendanceSheetService(current_app.logger)
 
 from ..resources.attendance_records_dto import CreateAttendanceRecordsDTO
 from ..resources.child_dto import ChildDTO
 from ..resources.visit_dto import VisitDTO
 from ..services.implementations.attendance_record_service import AttendanceRecordService
+attendance_sheet_service = AttendanceSheetService(current_app.logger)
 
 attendance_record_service = AttendanceRecordService(current_app.logger)
-attendance_sheet_service = AttendanceSheetService(current_app.logger)
 blueprint = Blueprint("visit", __name__, url_prefix="/visit")
 
+@blueprint.route("/", methods=["DELETE"], strict_slashes=False)
+def delete_visit():
+    args = request.args
+    id = int(args.get("id"))
+    if id:
+        try:
+            attendance_sheet_service.delete_attendance_sheet(id)
+
+            return f"Attendance sheet with id: {id} has been deleted", 200
+        except Exception as error:
+            return jsonify({"error": str(error)}), 500
+    else:
+        return jsonify({"error": "Must supply id as a query parameter."}), 400
 
 @blueprint.route("/", methods=["DELETE"], strict_slashes=False)
 def delete_visit():
@@ -156,11 +166,11 @@ def delete_visit():
     id = int(args.get("id"))
     if id:
         try:
-            attendace_sheet_service.delete_attendance_sheet(id)
+            attendance_sheet_service.delete_attendance_sheet(id)
 
             return f"Attendance sheet with id: {id} has been deleted", 200
         except Exception as error:
             return jsonify({"error": str(error)}), 500
     else:
         return jsonify({"error": "Must supply id as a query parameter."}), 400
-    
+

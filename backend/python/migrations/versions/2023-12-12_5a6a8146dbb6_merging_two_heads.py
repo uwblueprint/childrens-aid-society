672674581cecaa6df0f616d2c9bf286ca7175f6a
Eugene Zhang <zhang.eug@gmail.com>
2023-12-11 22:13:48 -0500

    Lint

--- backend/python/migrations/versions/2023-12-12_5a6a8146dbb6_merging_two_heads.py
+++ backend/python/migrations/versions/2023-12-12_5a6a8146dbb6_merging_two_heads.py
@@ -5,9 +5,8 @@ Revises: db7c2f31d3ea, 4df756383780, a52b5c1d2560
 Create Date: 2023-12-12 03:08:38.149095
 
 """
-from alembic import op
 import sqlalchemy as sa
-
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "5a6a8146dbb6"

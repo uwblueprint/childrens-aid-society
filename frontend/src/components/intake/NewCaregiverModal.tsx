05acf51b8079d3526aa16e0cf07425f13c9d0964
Malvika Patel <malvika.patel510@gmail.com>
2024-02-19 19:21:16 -0500

    added validate phone number functions

--- frontend/src/components/intake/NewCaregiverModal.tsx
+++ frontend/src/components/intake/NewCaregiverModal.tsx
@@ -122,6 +122,29 @@ const NewCaregiverModal = ({
     }
   }
 
+  function validatePrimaryPhoneNo(value: string) {
+    if (!value) {
+      setPrimaryPhoneNoError("Required");
+      setButtonDisabled(true);
+    } else if (!/^(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4}[,]?)(\s?([E|e]xt[.]?)(\s?\d+))?/.test(value)) {
+      setPrimaryPhoneNoError("Invalid phone number");
+      setButtonDisabled(true);
+    } else {
+      setPrimaryPhoneNoError(null);
+      setButtonDisabled(false);
+    }
+  }
+
+  function validateSecondaryPhoneNo(value: string) {
+    if (!/^(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4}[,]?)(\s?([E|e]xt[.]?)(\s?\d+))?/.test(value)) {
+      setSecondaryPhoneNoError("Invalid phone number");
+      setButtonDisabled(true);
+    } else {
+      setSecondaryPhoneNoError(null);
+      setButtonDisabled(false);
+    }
+  }
+
   return (
     <Box>
       <ModalComponent

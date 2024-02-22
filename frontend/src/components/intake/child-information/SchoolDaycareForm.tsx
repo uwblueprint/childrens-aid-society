16e1be6089e13205e06d1d6375e82ce6cd8c0ee9
Vaaranan Yogalingam <vaaranan.y@gmail.com>
2024-02-21 20:43:45 -0500

    Testing linting changes to SchoolDaycareForm

--- frontend/src/components/intake/child-information/SchoolDaycareForm.tsx
+++ frontend/src/components/intake/child-information/SchoolDaycareForm.tsx
@@ -31,17 +31,17 @@ const SchoolDaycareForm = ({
     setSchoolDetails(values);
   };
 
-const [schoolPhoneNoError, setschoolPhoneNoError] = useState<string | null>(null);
+  const [schoolPhoneNoError, setschoolPhoneNoError] = useState<string | null>(null);
 
-function validatePhone(value: string) {
-  if (!value) {
-    setschoolPhoneNoError("Required");
-  } else if (!/^(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4}[,]?)(\s?([E|e]xt[.]?)(\s?\d+))?/.test(value)) {
-    setschoolPhoneNoError("Invalid phone number");
-  } else {
-    setschoolPhoneNoError(null);
+  function validatePhone(value: string) {
+    if (!value) {
+      setschoolPhoneNoError("Required");
+    } else if (!/^(\+\d{1,3}\s?)?((\(\d{3}\)\s?)|(\d{3})(\s|-?))(\d{3}(\s|-?))(\d{4}[,]?)(\s?([E|e]xt[.]?)(\s?\d+))?/.test(value)) {
+      setschoolPhoneNoError("Invalid phone number");
+    } else {
+      setschoolPhoneNoError(null);
+    }
   }
-}
 
   return (
     <Formik
@@ -50,88 +50,88 @@ function validatePhone(value: string) {
       onSubmit={onSubmit}
     >
       {({ errors, touched }) => (
-      <Form style={{ padding: "2rem 12rem" }}>
-        <FormControl style={{ padding: "30px" }}>
-          <SimpleGrid columns={2} spacingX="3rem" spacingY="0.75rem">
-            <Box>
-              <FormLabel htmlFor="schoolName">NAME</FormLabel>
+        <Form style={{ padding: "2rem 12rem" }}>
+          <FormControl style={{ padding: "30px" }}>
+            <SimpleGrid columns={2} spacingX="3rem" spacingY="0.75rem">
+              <Box>
+                <FormLabel htmlFor="schoolName">NAME</FormLabel>
+                <Field
+                  as={CustomInput}
+                  id="schoolName"
+                  name="schoolName"
+                  type="string"
+                  placeholder="Enter name of school or daycare"
+                  icon={<Icon as={Home} />}
+                  onKeyUp={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setSchoolDetails({
+                      ...schoolDetails,
+                      schoolName: e.target.value,
+                    })
+                  }
+                />
+              </Box>
+              <Box>
+                <FormLabel htmlFor="schoolPhoneNo">
+                  SCHOOL/DAYCARE CONTACT INFORMATION
+                </FormLabel>
+                <Field
+                  as={CustomInput}
+                  name="schoolPhoneNo"
+                  id="schoolPhoneNo"
+                  type="string"
+                  placeholder="e.g. 555-555-5555"
+                  icon={<Icon as={Phone} />}
+                  onKeyUp={(e: React.ChangeEvent<HTMLInputElement>) =>
+                    setSchoolDetails({
+                      ...schoolDetails,
+                      schoolPhoneNo: e.target.value,
+                    })
+                  }
+                  validate={validatePhone}
+                />
+                {schoolPhoneNoError && (
+                  <div style={{ color: "red" }}>{schoolPhoneNoError}</div>
+                )}
+              </Box>
+            </SimpleGrid>
+            <Box paddingTop="10px">
+              <FormLabel htmlFor="schoolAddress">ADDRESS</FormLabel>
               <Field
                 as={CustomInput}
-                id="schoolName"
-                name="schoolName"
+                id="schoolAddress"
+                name="schoolAddress"
                 type="string"
-                placeholder="Enter name of school or daycare"
-                icon={<Icon as={Home} />}
+                placeholder="Enter address of school or daycare"
+                icon={<Icon as={Navigation} />}
                 onKeyUp={(e: React.ChangeEvent<HTMLInputElement>) =>
                   setSchoolDetails({
                     ...schoolDetails,
-                    schoolName: e.target.value,
+                    schoolAddress: e.target.value,
                   })
                 }
               />
             </Box>
-            <Box>
-              <FormLabel htmlFor="schoolPhoneNo">
-                SCHOOL/DAYCARE CONTACT INFORMATION
+            <Box paddingTop="10px">
+              <FormLabel htmlFor="dismissalTime">
+                DISMISSAL TIME <OptionalLabel />
               </FormLabel>
               <Field
                 as={CustomInput}
-                name="schoolPhoneNo"
-                id="schoolPhoneNo"
+                id="dismissalTime"
+                name="dismissalTime"
                 type="string"
-                placeholder="e.g. 555-555-5555"
-                icon={<Icon as={Phone} />}
+                placeholder="Enter dismissal time of school or daycare"
+                icon={<Icon as={Clock} />}
                 onKeyUp={(e: React.ChangeEvent<HTMLInputElement>) =>
                   setSchoolDetails({
                     ...schoolDetails,
-                    schoolPhoneNo: e.target.value,
+                    dismissalTime: e.target.value,
                   })
                 }
-                validate={validatePhone}
               />
-              {schoolPhoneNoError && (
-                  <div style={{ color: "red" }}>{schoolPhoneNoError}</div>
-              )}
             </Box>
-          </SimpleGrid>
-          <Box paddingTop="10px">
-            <FormLabel htmlFor="schoolAddress">ADDRESS</FormLabel>
-            <Field
-              as={CustomInput}
-              id="schoolAddress"
-              name="schoolAddress"
-              type="string"
-              placeholder="Enter address of school or daycare"
-              icon={<Icon as={Navigation} />}
-              onKeyUp={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setSchoolDetails({
-                  ...schoolDetails,
-                  schoolAddress: e.target.value,
-                })
-              }
-            />
-          </Box>
-          <Box paddingTop="10px">
-            <FormLabel htmlFor="dismissalTime">
-              DISMISSAL TIME <OptionalLabel />
-            </FormLabel>
-            <Field
-              as={CustomInput}
-              id="dismissalTime"
-              name="dismissalTime"
-              type="string"
-              placeholder="Enter dismissal time of school or daycare"
-              icon={<Icon as={Clock} />}
-              onKeyUp={(e: React.ChangeEvent<HTMLInputElement>) =>
-                setSchoolDetails({
-                  ...schoolDetails,
-                  dismissalTime: e.target.value,
-                })
-              }
-            />
-          </Box>
-        </FormControl>
-      </Form>
+          </FormControl>
+        </Form>
       )}
     </Formik>
   );

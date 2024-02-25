c75a85dedff6ec7551301885d42e22411535a10b
Helios He <helioshe4@gmail.com>
2024-02-25 10:57:47 -0800

    Add CreateVisitDTO class

--- backend/python/app/resources/visit_dto.py
+++ backend/python/app/resources/visit_dto.py
@@ -8,5 +8,26 @@ class VisitDTO(object):
         self.notes = kwargs.get("notes")
         self.childAndFamilySupportWorker = kwargs.get("childAndFamilySupportWorker")
 
+class CreateVisitDTO(object):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
     def validate(self):
-        pass
+        error_list = []
+        
+        if not self.user_id or not isinstance(self.user_id, int):
+            error_list.append("user_id is invalid")
+        if not self.childInformation:
+            error_list.append("childInformation is invalid")
+        if not self.visitTimestamp:
+            error_list.append("visitTimestamp is invalid")
+        if not self.attendance:
+            error_list.append("attendance is invalid")
+        if not self.transportation:
+            error_list.append("transportation is invalid")
+        if not self.notes:
+            error_list.append("notes is invalid")
+        if not self.childAndFamilySupportWorker:
+            error_list.append("childAndFamilySupportWorker is invalid")
+        
+        return error_list
